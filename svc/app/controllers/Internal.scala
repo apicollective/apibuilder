7917d6c2b75f956669dd00891c96886f102410c8
Michael Bryzek <michael@gilt.com>
2014-04-09 16:41:40 -0400

    Remove route generator from service

--- svc/app/controllers/Internal.scala
+++ svc/app/controllers/Internal.scala
@@ -1,16 +1,9 @@
 package controllers
 
-import lib.RouteGenerator
-
 import play.api._
 import play.api.mvc._
 
 object Internal extends Controller {
-
-  def routes() = Action { request =>
-    val generator = RouteGenerator.fromFile("./svc/api.json")
-    Ok(generator.generate())
-  }
 
   def healthcheck() = Action { request =>
     Ok("healthy")
