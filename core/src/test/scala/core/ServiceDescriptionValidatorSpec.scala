266fe1631712b82aea89cf6c62707ae78499757a
Michael Bryzek <michael@gilt.com>
2014-04-30 08:22:56 -0400

    Add multiple support to operation parameters

--- core/src/test/scala/core/ServiceDescriptionValidatorSpec.scala
+++ core/src/test/scala/core/ServiceDescriptionValidatorSpec.scala
@@ -166,7 +166,7 @@ class ServiceDescriptionValidatorSpec extends FunSpec with Matchers {
     validator.isValid should be(true)
   }
 
-  it("support ararys as types") {
+  it("support arrays as types in fields") {
     val json = """
     {
       "base_url": "http://localhost:9000",
@@ -186,6 +186,48 @@ class ServiceDescriptionValidatorSpec extends FunSpec with Matchers {
     val fields = validator.serviceDescription.get.resources.head.fields
     fields.find { _.name == "guid" }.get.multiple should be(false)
     fields.find { _.name == "tags" }.get.multiple should be(true)
+  }
+
+
+  it("support arrays as types in operations") {
+    val json = """
+    {
+      "base_url": "http://localhost:9000",
+      "name": "Api Doc",
+      "resources": {
+        "vendors": {
+          "fields": [
+            { "name": "guid", "type": "string" }
+          ],
+          "operations": [
+            {
+              "method": "POST",
+              "parameters": [
+                { "name": "guid", "type": "string" },
+                { "name": "tag", "type": "string", "multiple": true, "required": false }
+              ],
+              "responses": [
+                { "code": 200, "result": "vendors" }
+              ]
+            }
+          ]
+        }
+      }
+    }
+    """
+    val validator = ServiceDescriptionValidator(json)
+    validator.errors.mkString should be("")
+    val operation = validator.serviceDescription.get.resources.head.operations.head
+    operation.method should be("POST")
+    operation.parameters.find { _.name == "guid" }.get.multiple should be(false)
+
+    val guid = operation.parameters.find { _.name == "guid" }.get
+    guid.multiple should be(false)
+    guid.required should be(true)
+
+    val tag = operation.parameters.find { _.name == "tag" }.get
+    tag.multiple should be(true)
+    tag.required should be(false)
   }
 
 }
