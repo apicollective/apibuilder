ebd394c3a87c884f1def0af06ab6fe2269b41447
Gary Coady <gary@lyranthe.org>
2014-04-30 14:51:40 +0100

    Single build.sbt which can build multiple Play releases.

--- core/src/test/scala/core/generator/Play2RouteGeneratorSpec.scala
+++ core/src/test/scala/core/generator/Play2RouteGeneratorSpec.scala
@@ -2,12 +2,11 @@ package core.generator
 
 import core.ServiceDescription
 import org.scalatest.FlatSpec
-import org.junit.Assert._
 
 class Play2RouteGeneratorSpec extends FlatSpec {
 
   it should "generate routes" in {
-    val json = scala.io.Source.fromFile("../svc/api.json").getLines.mkString("\n")
+    val json = io.Source.fromURL(getClass.getResource("/api.json")).getLines.mkString("\n")
     val generator = Play2RouteGenerator(ServiceDescription(json))
     println(generator.generate())
   }
