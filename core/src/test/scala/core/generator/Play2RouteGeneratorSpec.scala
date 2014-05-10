49ef8bf44f3b09244bbffc1cc1e65ea69f579555
Michael Bryzek <michael@gilt.com>
2014-05-10 08:23:22 -0400

    Remove NPE if resource not found

--- core/src/test/scala/core/generator/Play2RouteGeneratorSpec.scala
+++ core/src/test/scala/core/generator/Play2RouteGeneratorSpec.scala
@@ -6,7 +6,11 @@ import org.scalatest.FlatSpec
 class Play2RouteGeneratorSpec extends FlatSpec {
 
   it should "generate routes" in {
-    val json = io.Source.fromURL(getClass.getResource("/api.json")).getLines.mkString("\n")
+    val r = getClass.getResource("/api.json")
+    if (r == null) {
+      sys.error("Could not find resource named api.json")
+    }
+    val json = io.Source.fromURL(r).getLines.mkString("\n")
     val generator = Play2RouteGenerator(ServiceDescription(json))
     println(generator.generate())
   }
