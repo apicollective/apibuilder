7a926661979fec92e5f6e1891c03bd0fc3ac135e
Michael Bryzek <michael@gilt.com>
2014-05-21 12:02:07 -0400

    Only delete catalog publisher service

--- schema/scripts/20140521-092810.sql
+++ schema/scripts/20140521-092810.sql
@@ -7,7 +7,8 @@ update services
    set deleted_at = now(),
        deleted_by_guid = (select guid from users where deleted_at is null and email='admin@apidoc.me')
  where deleted_at is null
-   and created_at < '2014-05-21 10:27:07.047495+00';
+   and created_at < '2014-05-21 10:27:07.047495+00'
+   and name = 'Catalog Publisher';
 
 update versions
    set deleted_at = now(),
