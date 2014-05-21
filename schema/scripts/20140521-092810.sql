8d82f1a880c885adfad509ca4fec14c3fa8bdd48
Michael Bryzek <michael@gilt.com>
2014-05-21 09:28:19 -0400

    Delete old service versions on past api.json format

---
+++ schema/scripts/20140521-092810.sql
@@ -0,0 +1,16 @@
+update users
+   set email = 'admin@apidoc.me'
+ where email = 'admin@apidoc.com'
+   and deleted_at is null;
+
+update services
+   set deleted_at = now(),
+       deleted_by_guid = (select guid from users where deleted_at is null and email='admin@apidoc.me')
+ where deleted_at is null
+   and created_at < '2014-05-21 10:27:07.047495+00';
+
+update versions
+   set deleted_at = now(),
+       deleted_by_guid = (select guid from users where deleted_at is null and email='admin@apidoc.me')
+ where deleted_at is null
+   and created_at < '2014-05-21 10:27:07.047495+00';
