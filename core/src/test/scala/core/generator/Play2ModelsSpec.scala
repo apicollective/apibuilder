0bdb0c81789f3ab62d1882de7c577cf95d8a24e1
Michael Reed <mreed@giltcity.com>
2014-05-31 17:35:02 -0400

    align play2 models and client specs with other specs
    - just print the code out for now

--- core/src/test/scala/core/generator/Play2ModelsSpec.scala
+++ core/src/test/scala/core/generator/Play2ModelsSpec.scala
@@ -9,24 +9,7 @@ import org.scalatest.FlatSpec
 
 class Play2ModelsSpec extends FlatSpec {
   "Play2Models" should "generate scala" in {
-    val json = scala.io.Source.fromFile("/web/svc-iris-hub/api.json").getLines.mkString("\n")
-    val src = Play2Models(json)
-    val dir = new File("/web/iris-hub-client/src/main/scala/irishub")
-    dir.mkdirs
-    val pw = new PrintWriter(new File(dir, "Client.scala"))
-    try pw.println(src)
-    pw.close()
-    val token = sys.props.getOrElse("irishub.api.token", "")
-    val url = sys.props.getOrElse("irishub.api.url", "")
-    val proc = Process(
-      Seq(
-        "sbt",
-        s"-Dirishub.api.token=${token}",
-        s"-Dirishub.api.url=${url}",
-        "compile"
-      ),
-      new File("/web/iris-hub-client")
-    )
-    assert(proc.! == 0)
+    val json = io.Source.fromURL(getClass.getResource("/api.json")).getLines.mkString("\n")
+    println(Play2Models(json))
   }
 }
