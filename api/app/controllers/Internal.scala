a33480e1c11113abef115ed933af830e16873715
Michael Bryzek <michael@gilt.com>
2014-06-23 10:30:31 -0400

    Update Healthchecks to actually execute dependencies

--- api/app/controllers/Internal.scala
+++ api/app/controllers/Internal.scala
@@ -1,12 +1,17 @@
 package controllers
 
+import db.OrganizationDao
 import play.api._
 import play.api.mvc._
+import play.api.libs.json._
 
 object Internal extends Controller {
 
+  private val Result = Json.toJson(Map("status" -> "healthy"))
+
   def healthcheck() = Action { request =>
-    Ok("healthy")
+    val orgs = OrganizationDao.findAll(limit = 1).headOption
+    Ok(Result)
   }
 
 }
--- www/app/controllers/Internal.scala
+++ www/app/controllers/Internal.scala
@@ -5,8 +5,14 @@ import play.api.mvc._
 
 object Internal extends Controller {
 
-  def healthcheck() = Action { request =>
-    Ok("healthy")
+  implicit val context = scala.concurrent.ExecutionContext.Implicits.global
+
+  def healthcheck() = Action.async { request =>
+    for {
+      orgs <- Authenticated.api.Organizations.get(limit = Some(1))
+    } yield {
+      Ok(views.html._internal_.healthcheck(orgs.entity.headOption))
+    }
   }
 
 }
