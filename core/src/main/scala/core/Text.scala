eda30e9e9e3ac9370f29ec8ecfdbd56d58df439c
Michael Bryzek <michael@gilt.com>
2014-08-04 10:02:06 -0400

    To not indent blank lines (just adds empty space)

--- core/src/main/scala/core/Text.scala
+++ core/src/main/scala/core/Text.scala
@@ -156,7 +156,13 @@ object Text {
   implicit class Indentable(s: String) {
     def indent: String = indent(2)
     def indent(width: Int): String = {
-      s.split("\n").map((" " * width) + _).mkString("\n")
+      s.split("\n").map { value =>
+        if (value.trim == "") {
+          ""
+        } else {
+          (" " * width) + value
+        }
+      }.mkString("\n")
     }
   }
 
