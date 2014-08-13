d5d699c0128cad23ef5248a49abe63f95d1ea70a
Michael Bryzek <michael@gilt.com>
2014-08-13 09:33:35 +0100

    Introduce validations endpoint (public) to validate json docs via an API

---
+++ api/app/controllers/Validations.scala
@@ -0,0 +1,25 @@
+package controllers
+
+import lib.{Validation, ValidationError}
+import core.{ServiceDescription, ServiceDescriptionValidator}
+import db.VersionForm
+import play.api.mvc._
+import play.api.libs.json._
+
+object Validations extends Controller {
+
+  case class ValidationResult(valid: Boolean, errors: Seq[String])
+
+  object ValidationResult {
+    implicit val validationResultWrites = Json.writes[ValidationResult]
+  }
+
+  def post() = Action(parse.temporaryFile) { request =>
+    val contents = scala.io.Source.fromFile(request.body.file).getLines.mkString("\n")
+    ServiceDescriptionValidator(contents).errors match {
+      case Nil => Ok(Json.toJson(ValidationResult(true, Nil)))
+      case errors => Ok(Json.toJson(ValidationResult(false, errors)))
+    }
+  }
+
+}
