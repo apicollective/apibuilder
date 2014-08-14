9a627d3cabeb4c0af5abeff486c89d024d2f324e
Michael Bryzek <michael@gilt.com>
2014-08-14 18:22:02 -0400

    Revert "Play client: Potential bug fix for string - encoding as JsValue for body type string"
    This reverts commit c084bee9ab0a2db4272e4513a7bb593268fa34ba.

--- core/src/main/scala/core/generator/Play2Util.scala
+++ core/src/main/scala/core/generator/Play2Util.scala
@@ -1,296 +1,116 @@
-package com.gilt.svcavroschemaregistry.models {
-  case class Errors(
-    messages: scala.collection.Seq[String]
-  )
+package core.generator
 
-  /**
-   * An Avro schema together with it's subject and fingerprint
-   */
-  case class SchemaDetails(
-    subject: String,
-    fingerprint: String,
-    schema: String
-  )
+import core._
+import Text._
 
-  /**
-   * A single event data type, which may evolve through multiple schemas
-   */
-  case class Subject(
-    subject: String
-  )
+object Play2Util {
+  import ScalaDataType._
 
-
-}
-
-package com.gilt.svcavroschemaregistry.models {
-  package object json {
-    import play.api.libs.json.__
-    import play.api.libs.json.JsString
-    import play.api.libs.json.Writes
-    import play.api.libs.functional.syntax._
-
-    private[svcavroschemaregistry] implicit val jsonReadsUUID = __.read[String].map(java.util.UUID.fromString)
-
-    private[svcavroschemaregistry] implicit val jsonWritesUUID = new Writes[java.util.UUID] {
-      def writes(x: java.util.UUID) = JsString(x.toString)
-    }
-
-    private[svcavroschemaregistry] implicit val jsonReadsJodaDateTime = __.read[String].map { str =>
-      import org.joda.time.format.ISODateTimeFormat.dateTimeParser
-      dateTimeParser.parseDateTime(str)
-    }
-
-    private[svcavroschemaregistry] implicit val jsonWritesJodaDateTime = new Writes[org.joda.time.DateTime] {
-      def writes(x: org.joda.time.DateTime) = {
-        import org.joda.time.format.ISODateTimeFormat.dateTime
-        val str = dateTime.print(x)
-        JsString(str)
+  def queryParams(op: ScalaOperation): Option[String] = {
+    if (op.queryParameters.isEmpty) {
+      None
+    } else {
+      val arrayParams = op.queryParameters.filter(_.multiple) match {
+        case Nil => Seq.empty
+        case params => {
+          params.map { p =>
+            if (p.isOption) {
+              s"""  ${p.name}.getOrElse(Seq.empty).map("${p.originalName}" -> ${ScalaDataType.asString("_", p.baseType)})"""
+            } else {
+              s"""  ${p.name}.map("${p.originalName}" -> ${ScalaDataType.asString("_", p.baseType)})"""
+            }
+          }
+        }
       }
-    }
+      val arrayParamString = arrayParams.mkString(" ++\n")
 
+      val singleParams = op.queryParameters.filter(!_.multiple) match {
+        case Nil => Seq.empty
+        case params => {
+          Seq(
+            "val query = Seq(",
+            params.map { p =>
+              if (p.isOption) {
+                s"""  ${p.name}.map("${p.originalName}" -> ${ScalaDataType.asString("_", p.baseType)})"""
+              } else {
+                s"""  Some("${p.originalName}" -> ${ScalaDataType.asString(p.name, p.baseType)})"""
+              }
+            }.mkString(",\n"),
+            ").flatten"
+          )
+        }
+      }
+      val singleParamString = singleParams.mkString("\n")
 
-    implicit def jsonReadssvcavroschemaregistryErrors: play.api.libs.json.Reads[Errors] = {
-      (__ \ "messages").readNullable[scala.collection.Seq[String]].map(_.getOrElse(Nil)).map { x => new Errors(messages = x) }
-    }
-
-    implicit def jsonWritessvcavroschemaregistryErrors: play.api.libs.json.Writes[Errors] = new play.api.libs.json.Writes[Errors] {
-      def writes(x: Errors) = play.api.libs.json.Json.obj(
-        "messages" -> play.api.libs.json.Json.toJson(x.messages)
-      )
-    }
-
-    implicit def jsonReadssvcavroschemaregistrySchemaDetails: play.api.libs.json.Reads[SchemaDetails] = {
-      (
-        (__ \ "subject").read[String] and
-        (__ \ "fingerprint").read[String] and
-        (__ \ "schema").read[String]
-      )(SchemaDetails.apply _)
-    }
-
-    implicit def jsonWritessvcavroschemaregistrySchemaDetails: play.api.libs.json.Writes[SchemaDetails] = {
-      (
-        (__ \ "subject").write[String] and
-        (__ \ "fingerprint").write[String] and
-        (__ \ "schema").write[String]
-      )(unlift(SchemaDetails.unapply _))
-    }
-
-    implicit def jsonReadssvcavroschemaregistrySubject: play.api.libs.json.Reads[Subject] = {
-      (__ \ "subject").read[String].map { x => new Subject(subject = x) }
-    }
-
-    implicit def jsonWritessvcavroschemaregistrySubject: play.api.libs.json.Writes[Subject] = new play.api.libs.json.Writes[Subject] {
-      def writes(x: Subject) = play.api.libs.json.Json.obj(
-        "subject" -> play.api.libs.json.Json.toJson(x.subject)
+      Some(
+        if (singleParams.isEmpty) {
+          "val query = " + arrayParamString.trim
+        } else if (arrayParams.isEmpty) {
+          singleParamString
+        } else {
+          singleParamString + " ++\n" + arrayParamString
+        }
       )
     }
   }
-}
-
-package com.gilt.svcavroschemaregistry {
-  object helpers {
-    import org.joda.time.DateTime
-    import org.joda.time.format.ISODateTimeFormat
-    import play.api.mvc.QueryStringBindable
-
-    import scala.util.{ Failure, Success, Try }
-
-    private[helpers] val dateTimeISOParser = ISODateTimeFormat.dateTimeParser()
-    private[helpers] val dateTimeISOFormatter = ISODateTimeFormat.dateTime()
-
-    private[helpers] def parseDateTimeISO(s: String): Either[String, DateTime] = {
-      Try(dateTimeISOParser.parseDateTime(s)) match {
-        case Success(dt) => Right(dt)
-        case Failure(f) => Left("Could not parse DateTime: " + f.getMessage)
-      }
+  
+  def pathParams(op: ScalaOperation): String = {
+    val pairs = op.pathParameters.map { p =>
+      require(!p.multiple, "Path parameters cannot be lists.")
+      p.originalName -> PathParamHelper.urlEncode(p.name, p.datatype)
     }
+    val tmp: String = pairs.foldLeft(op.path) {
+      case (path, (name, value)) =>
+        val spec = s"/:$name"
+        val from = path.indexOfSlice(spec)
+        path.patch(from, s"/$${$value}", spec.length)
+    }
+    s""" s"$tmp" """.trim
+  }
 
-    implicit object DateTimeISOQueryStringBinder extends QueryStringBindable[DateTime] {
-      override def bind(key: String, params: Map[String, Seq[String]]): Option[Either[String, DateTime]] = {
-        for {
-          values <- params.get(key)
-          s <- values.headOption
-        } yield parseDateTimeISO(s)
-      }
+  def formBody(op: ScalaOperation): Option[String] = {
+    // Can have both or form params but not both as we can only send a single document
+    assert(op.body.isEmpty || op.formParameters.isEmpty)
 
-      override def unbind(key: String, time: DateTime): String = key + "=" + dateTimeISOFormatter.print(time)
+    if (op.formParameters.isEmpty && op.body.isEmpty) {
+      None
+
+    } else if (!op.body.isEmpty) {
+      Some(
+        op.body.get.body match {
+          case PrimitiveBody(dt) => {
+            s"val payload = " + ScalaDataType.asString(Text.initLowerCase(op.body.get.name), ScalaDataType(dt))
+          }
+          case ModelBody(name) => s"val payload = play.api.libs.json.Json.toJson(${Text.initLowerCase(op.body.get.name)})"
+          case EnumBody(name) => s"val payload = ${Text.initLowerCase(op.body.get.name)}.toString"
+        }
+      )
+
+    } else {
+      val params = op.formParameters.map { param =>
+        s""" "${param.originalName}" -> play.api.libs.json.Json.toJson(${param.name})""".trim
+      }.mkString(",\n")
+      Some(
+        Seq(
+          "val payload = play.api.libs.json.Json.obj(",
+          params.indent,
+          ")"
+        ).mkString("\n")
+      )
     }
   }
 
-  class Client(apiUrl: String, apiToken: scala.Option[String] = None) {
-    import com.gilt.svcavroschemaregistry.models._
-    import com.gilt.svcavroschemaregistry.models.json._
-
-    private val logger = play.api.Logger("com.gilt.svcavroschemaregistry.client")
-
-    logger.info(s"Initializing com.gilt.svcavroschemaregistry.client for url $apiUrl")
-
-    def subjects: Subjects = Subjects
-
-    trait Subjects {
-      /**
-       * Gets all subjects which have a schema registered.
-       */
-      def get()(implicit ec: scala.concurrent.ExecutionContext): scala.concurrent.Future[scala.collection.Seq[com.gilt.svcavroschemaregistry.models.Subject]]
-
-      /**
-       * Registers a new schema version for the specified subject. The body should
-       * contain a text representation of the schema (not a SchemaDetails).  A new schema
-       * which violates evolution rules will result in a 409-Conflict response.
-       */
-      def putSchemasBySubject(value: String, 
-        subject: String
-      )(implicit ec: scala.concurrent.ExecutionContext): scala.concurrent.Future[com.gilt.svcavroschemaregistry.models.SchemaDetails]
-
-      /**
-       * Gets a list of all schemas registered for this subject.
-       */
-      def getSchemasBySubject(
-        subject: String
-      )(implicit ec: scala.concurrent.ExecutionContext): scala.concurrent.Future[scala.collection.Seq[com.gilt.svcavroschemaregistry.models.SchemaDetails]]
-
-      /**
-       * Gets the schema with the specified fingerprint
-       */
-      def getSchemasBySubjectAndFingerprint(
-        subject: String,
-        fingerprint: String
-      )(implicit ec: scala.concurrent.ExecutionContext): scala.concurrent.Future[scala.Option[com.gilt.svcavroschemaregistry.models.SchemaDetails]]
-
-      /**
-       * Gets the latest schema for the specified subject
-       */
-      def getLatestSchemaBySubject(
-        subject: String
-      )(implicit ec: scala.concurrent.ExecutionContext): scala.concurrent.Future[scala.Option[com.gilt.svcavroschemaregistry.models.SchemaDetails]]
-    }
-
-    object Subjects extends Subjects {
-      override def get()(implicit ec: scala.concurrent.ExecutionContext): scala.concurrent.Future[scala.collection.Seq[com.gilt.svcavroschemaregistry.models.Subject]] = {
-        GET(s"/subjects").map {
-          case r if r.status == 200 => r.json.as[scala.collection.Seq[com.gilt.svcavroschemaregistry.models.Subject]]
-          case r => throw new FailedRequest(r)
+  private object PathParamHelper {
+    def urlEncode(name: String, d: ScalaDataType): String = {
+      d match {
+        case ScalaStringType => s"""play.utils.UriEncoding.encodePathSegment($name, "UTF-8")"""
+        case ScalaIntegerType | ScalaDoubleType | ScalaLongType | ScalaBooleanType | ScalaDecimalType | ScalaUuidType => name
+        case ScalaEnumType(_) => s"""play.utils.UriEncoding.encodePathSegment($name.toString, "UTF-8")"""
+        case t => {
+          sys.error(s"Cannot encode params of type[$t] as path parameters (name: $name)")
         }
       }
-
-      override def putSchemasBySubject(value: String, 
-        subject: String
-      )(implicit ec: scala.concurrent.ExecutionContext): scala.concurrent.Future[com.gilt.svcavroschemaregistry.models.SchemaDetails] = {
-        val payload = play.api.libs.json.Json.toJson(value)
-
-        PUT(s"/subjects/${play.utils.UriEncoding.encodePathSegment(subject, "UTF-8")}/schemas", payload).map {
-          case r if r.status == 201 => r.json.as[com.gilt.svcavroschemaregistry.models.SchemaDetails]
-          case r if r.status == 409 => throw new com.gilt.svcavroschemaregistry.error.ErrorsResponse(r)
-          case r => throw new FailedRequest(r)
-        }
-      }
-
-      override def getSchemasBySubject(
-        subject: String
-      )(implicit ec: scala.concurrent.ExecutionContext): scala.concurrent.Future[scala.collection.Seq[com.gilt.svcavroschemaregistry.models.SchemaDetails]] = {
-        GET(s"/subjects/${play.utils.UriEncoding.encodePathSegment(subject, "UTF-8")}/schemas").map {
-          case r if r.status == 200 => r.json.as[scala.collection.Seq[com.gilt.svcavroschemaregistry.models.SchemaDetails]]
-          case r => throw new FailedRequest(r)
-        }
-      }
-
-      override def getSchemasBySubjectAndFingerprint(
-        subject: String,
-        fingerprint: String
-      )(implicit ec: scala.concurrent.ExecutionContext): scala.concurrent.Future[scala.Option[com.gilt.svcavroschemaregistry.models.SchemaDetails]] = {
-        GET(s"/subjects/${play.utils.UriEncoding.encodePathSegment(subject, "UTF-8")}/schemas/${play.utils.UriEncoding.encodePathSegment(fingerprint, "UTF-8")}").map {
-          case r if r.status == 200 => Some(r.json.as[com.gilt.svcavroschemaregistry.models.SchemaDetails])
-          case r if r.status == 404 => None
-          case r => throw new FailedRequest(r)
-        }
-      }
-
-      override def getLatestSchemaBySubject(
-        subject: String
-      )(implicit ec: scala.concurrent.ExecutionContext): scala.concurrent.Future[scala.Option[com.gilt.svcavroschemaregistry.models.SchemaDetails]] = {
-        GET(s"/subjects/${play.utils.UriEncoding.encodePathSegment(subject, "UTF-8")}/latestSchema").map {
-          case r if r.status == 200 => Some(r.json.as[com.gilt.svcavroschemaregistry.models.SchemaDetails])
-          case r if r.status == 404 => None
-          case r => throw new FailedRequest(r)
-        }
-      }
-    }
-
-    private val UserAgent = "apidoc:0.5.0 http://www.apidoc.me/gilt/code/svc-avro-schema-registry/0.0.1-dev/play_2_3_client"
-
-    def _requestHolder(path: String): play.api.libs.ws.WSRequestHolder = {
-      import play.api.Play.current
-
-      val holder = play.api.libs.ws.WS.url(apiUrl + path).withHeaders("User-Agent" -> UserAgent)
-      apiToken.fold(holder) { token =>
-        holder.withAuth(token, "", play.api.libs.ws.WSAuthScheme.BASIC)
-      }
-    }
-
-    def _logRequest(method: String, req: play.api.libs.ws.WSRequestHolder)(implicit ec: scala.concurrent.ExecutionContext): play.api.libs.ws.WSRequestHolder = {
-      val queryComponents = for {
-        (name, values) <- req.queryString
-        value <- values
-      } yield name -> value
-      val url = s"${req.url}${queryComponents.mkString("?", "&", "")}"
-      apiToken.fold(logger.info(s"curl -X $method $url")) { _ =>
-        logger.info(s"curl -X $method -u '[REDACTED]:' $url")
-      }
-      req
-    }
-
-    def POST(
-      path: String,
-      data: play.api.libs.json.JsValue = play.api.libs.json.Json.obj(),
-      q: Seq[(String, String)] = Seq.empty
-    )(implicit ec: scala.concurrent.ExecutionContext): scala.concurrent.Future[play.api.libs.ws.WSResponse] = {
-      _logRequest("POST", _requestHolder(path).withQueryString(q:_*)).post(data)
-    }
-
-    def GET(
-      path: String,
-      q: Seq[(String, String)] = Seq.empty
-    )(implicit ec: scala.concurrent.ExecutionContext): scala.concurrent.Future[play.api.libs.ws.WSResponse] = {
-      _logRequest("GET", _requestHolder(path).withQueryString(q:_*)).get()
-    }
-
-    def PUT(
-      path: String,
-      data: play.api.libs.json.JsValue = play.api.libs.json.Json.obj(),
-      q: Seq[(String, String)] = Seq.empty
-    )(implicit ec: scala.concurrent.ExecutionContext): scala.concurrent.Future[play.api.libs.ws.WSResponse] = {
-      _logRequest("PUT", _requestHolder(path).withQueryString(q:_*)).put(data)
-    }
-
-    def PATCH(
-      path: String,
-      data: play.api.libs.json.JsValue = play.api.libs.json.Json.obj(),
-      q: Seq[(String, String)] = Seq.empty
-    )(implicit ec: scala.concurrent.ExecutionContext): scala.concurrent.Future[play.api.libs.ws.WSResponse] = {
-      _logRequest("PATCH", _requestHolder(path).withQueryString(q:_*)).patch(data)
-    }
-
-    def DELETE(
-      path: String,
-      q: Seq[(String, String)] = Seq.empty
-    )(implicit ec: scala.concurrent.ExecutionContext): scala.concurrent.Future[play.api.libs.ws.WSResponse] = {
-      _logRequest("DELETE", _requestHolder(path).withQueryString(q:_*)).delete()
-    }
-
-  }
-
-  case class FailedRequest(response: play.api.libs.ws.WSResponse) extends Exception(response.status + ": " + response.body)
-
-  package error {
-
-    import com.gilt.svcavroschemaregistry.models.json._
-
-    case class ErrorsResponse(response: play.api.libs.ws.WSResponse) extends Exception(response.status + ": " + response.body) {
-
-      lazy val errors = response.json.as[com.gilt.svcavroschemaregistry.models.Errors]
-
     }
   }
-
 
 }
