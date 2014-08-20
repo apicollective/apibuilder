793c3c718a5ecaabf1de2a67bcb096e216766be8
Michael Bryzek <michael@gilt.com>
2014-08-20 12:34:46 -0400

    Revert "Make json for UUID and DateTime public to allow importing"
    This reverts commit 1ade8120d2d32b2600c55c3072b799091f2a75d6.

--- core/src/main/scala/core/generator/Play2Models.scala
+++ core/src/main/scala/core/generator/Play2Models.scala
@@ -24,18 +24,18 @@ package ${ssd.packageName}.models {
     import play.api.libs.json.Writes
     import play.api.libs.functional.syntax._
 
-    implicit val jsonReadsUUID = __.read[String].map(java.util.UUID.fromString)
+    private[${ssd.packageNamePrivate}] implicit val jsonReadsUUID = __.read[String].map(java.util.UUID.fromString)
 
-    implicit val jsonWritesUUID = new Writes[java.util.UUID] {
+    private[${ssd.packageNamePrivate}] implicit val jsonWritesUUID = new Writes[java.util.UUID] {
       def writes(x: java.util.UUID) = JsString(x.toString)
     }
 
-    implicit val jsonReadsJodaDateTime = __.read[String].map { str =>
+    private[${ssd.packageNamePrivate}] implicit val jsonReadsJodaDateTime = __.read[String].map { str =>
       import org.joda.time.format.ISODateTimeFormat.dateTimeParser
       dateTimeParser.parseDateTime(str)
     }
 
-    implicit val jsonWritesJodaDateTime = new Writes[org.joda.time.DateTime] {
+    private[${ssd.packageNamePrivate}] implicit val jsonWritesJodaDateTime = new Writes[org.joda.time.DateTime] {
       def writes(x: org.joda.time.DateTime) = {
         import org.joda.time.format.ISODateTimeFormat.dateTime
         val str = dateTime.print(x)
