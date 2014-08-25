6b8ce775de7d916dd37a055ddef2ac55f0911ba9
Michael Bryzek <michael@gilt.com>
2014-08-25 17:05:15 -0400

    Initial ruby support for client tests

--- client-tests/download-and-compile.rb
+++ client-tests/download-and-compile.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/env ruby
 
 load 'ruby_client.rb'
-CLIENT_DIR = "src/main/scala/clients"
 
 service_uri = ARGV.shift
 
@@ -11,55 +10,99 @@ if service_uri.to_s.strip == "" || token.to_s.strip == ""
 end
 
 orgs = ['gilt']
-#services = ['api-doc']
+services = ['api-doc']
+
+class RubyTester
+
+  CLIENT_DIR = "clients"
+
+  def file_extension
+    "rb"
+  end
+
+  def test
+    system("ruby #{CLIENT_DIR}/*.downloaded.rb")
+  end
+
+  def clean!(platform)
+    cmd = "rm -f #{platform}/#{CLIENT_DIR}/*.downloaded.rb"
+    puts cmd
+    system(cmd)
+  end
+
+  def write(platform, org_key, service_key, target_name, code)
+    filename = File.join(platform, CLIENT_DIR, [org_key, service_key, "#{target_name}.downloaded.rb"].join("."))
+    File.open(filename, "w") do |out|
+      out << "module Clienttests_#{target_name}\n\n"
+      out << code
+      out << "\n\nend"
+    end
+    filename
+  end
+
+end
+
+class ScalaTester
+
+  CLIENT_DIR = "src/main/scala/clients"
+
+  def test
+    system("sbt compile")
+  end
+
+  def clean!(platform)
+    cmd = "rm -f #{platform}/#{CLIENT_DIR}/*.downloaded.scala"
+    puts cmd
+    system(cmd)
+  end
+
+  def write(platform, org_key, service_key, target_name, code)
+    filename = File.join(platform, CLIENT_DIR, [org_key, service_key, "#{target_name}.downloaded.scala"].join("."))
+    File.open(filename, "w") do |out|
+      out << "package clienttests_#{target_name} {\n\n"
+      out << code
+      out << "\n\n}"
+    end
+    filename
+  end
+
+end
 
 class Target
 
-  attr_reader :platform, :test_command, :names
+  attr_reader :platform, :tester, :names
 
-  def initialize(platform, test_command, names)
+  def initialize(platform, tester, names)
     @platform = platform
-    @test_command = test_command
+    @tester = tester
     @names = names
   end
 
 end
 
-targets = [Target.new('play_2_2', 'sbt compile', ['play_2_2_client', 'play_2_x_json', 'scala_models']),
-           Target.new('play_2_3', 'sbt compile', ['play_2_3_client', 'play_2_x_json', 'scala_models'])]
-
-def write_target(platform, org, service, target, code)
-  filename = File.join(platform, CLIENT_DIR, [org.key, service.key, "#{target.value}.downloaded.scala"].join("."))
-  File.open(filename, "w") do |out|
-    out << "package clienttests_#{target.value} {\n\n"
-    out << code.source
-    out << "\n\n}"
-  end
-  filename
-end
+targets = [Target.new('ruby', RubyTester.new, ['ruby_client']),
+           Target.new('play_2_2', ScalaTester.new, ['play_2_2_client', 'play_2_x_json', 'scala_models']),
+           Target.new('play_2_3', ScalaTester.new, ['play_2_3_client', 'play_2_x_json', 'scala_models'])]
 
 def get_code(client, org, service, target)
   client.code.get_by_org_key_and_service_key_and_version_and_target(org.key, service.key, "latest", target)
 end
-
-cmd = "rm -f #{CLIENT_DIR}/*.downloaded.scala"
-puts cmd
-system(cmd)
 
 client = ApiDoc::Client.new(service_uri, :authorization => ApiDoc::HttpClient::Authorization.basic(token))
 
 targets.each do |target|
   puts "Platform: #{target.platform}"
   puts "--------------------------------------------------"
+  target.tester.clean!(target.platform)
   client.organizations.get.each do |org|
     next unless orgs.include?(org.key)
     client.services.get_by_org_key(org.key).each do |service|
-      #next unless services.include?(service.key)
+      next unless services.include?(service.key)
       puts "  %s/%s" % [org.key, service.key]
       target.names.each do |target_name|
         t = ApiDoc::Models::Target.send(target_name)
         if code = get_code(client, org, service, t)
-          filename = write_target(target.platform, org, service, t, code)
+          filename = target.tester.write(target.platform, org.key, service.key, t.value, code.source)
           puts "    #{t.value}: #{filename}"
         end
       end
@@ -67,12 +110,12 @@ targets.each do |target|
   end
 
   puts ""
-  puts "  cd ./#{target.platform} && #{target.test_command}"
+  puts "  Testing in ./#{target.platform}"
   Dir.chdir(target.platform) do
-    if system(target.test_command)
-      puts "  - All clients compiled"
+    if target.tester.test
+      puts "  - Client tests passed"
     else
-      puts "  - Clients failed to compile"
+      puts "  - Client tests failed"
       exit 1
     end
   end
