ffd48dbec5b5db9f487cbd68223fcdd156e27bb5
Michael Bryzek <michael@gilt.com>
2014-08-30 15:16:52 +0100

    Increment targets to beta

--- core/src/main/scala/core/generator/Target.scala
+++ core/src/main/scala/core/generator/Target.scala
@@ -27,37 +27,37 @@ object Target {
       key = "ruby_client",
       name = "Ruby client",
       description = "A pure ruby library to consume api.json web services. From apidoc, you can download a single file that provides a complete client to a service. The ruby client has minimal dependencies and does not require any additional gems.",
-      status = Status.Alpha
+      status = Status.Beta
     ),
     Target(
       key = "play_2_2_client",
       name = "Play 2.2 client",
       description = "Play Framework 2.2 client based on <a href='http://www.playframework.com/documentation/2.2.x/ScalaWS''>WS API</a>.",
-      status = Status.Alpha
+      status = Status.Beta
     ),
     Target(
       key = "play_2_3_client",
       name = "Play 2.3 client",
       description = "Play Framework 2.3 client based on  <a href='http://www.playframework.com/documentation/2.3.x/ScalaWS'>WS API</a>.",
-      status = Status.Alpha
+      status = Status.Beta
     ),
     Target(
       key = "play_2_x_json",
       name = "Play 2.x json",
       description = "Generate play 2.x case classes with json serialization based on <a href='http://www.playframework.com/documentation/2.3.x/ScalaJsonCombinators'>Scala Json combinators</a>. No need to use this target if you are already using the Play Client target.",
-      status = Status.Alpha
+      status = Status.Beta
     ),
     Target(
       key = "play_2_x_routes",
       name = "Play 2.x routes",
       description = "Apidoc can generate a routes file for the play 2.x framework. One workflow here is to write the service description in apidoc, upload, copy the routes file, then implement the controllers in play. By code generating the routes file, the compiler tells you if you are missing any operations defined by api.json. While this does not provide full testing (e.g. does not validate the responses), we have found it very pragmatic to ensure services match specifications.",
-      status = Status.Alpha
+      status = Status.Beta
     ),
     Target(
       key = "scala_models",
       name = "Scala models",
       description = "Generate scala models from the API description.",
-      status = Status.Alpha
+      status = Status.Beta
     ),
     Target(
       key = "avro_schema",
