cd007646352e739e3d0fc67418b565c4f5620c8a
Michael Bryzek <michael@gilt.com>
2014-09-17 21:17:28 -0400

    Add POST

--- client-tests/standalone/client.scala
+++ client-tests/standalone/client.scala
@@ -8,18 +8,16 @@ object Main extends App {
 
   val client = new Foo.Client("http://localhost:8001")
   try {
-    val result = Await.result(client.healthchecks.get(), 5 seconds)
-    println("RESULT: " + result)
+    println("healthcheck: " + Await.result(client.healthchecks.get(), 5 seconds).get)
+
+    val incidents = Await.result(client.incidents.get(limit = Some(5)), 5 seconds)
+    println("incidents: " + incidents.size)
+    incidents.foreach { i =>
+      println(" - " + i.id.toString)
+    }
+
   } finally {
     client.asyncHttpClient.close()
-  }
-
-}
-
-class Logger() {
-
-  def info(message: String) {
-    println(message)
   }
 
 }
@@ -29,36 +27,61 @@ object Foo {
     apiUrl: String,
     apiToken: scala.Option[String] = None
   ) {
+    import com.gilt.quality.models.json._
+
     val asyncHttpClient = new AsyncHttpClient()
-    val logger = new Logger()
+    val UserAgent = "TODO"
 
     def healthchecks: Healthchecks = Healthchecks
 
-    def _logRequest(
-      method: String,
-      path: String,
-      q: Seq[(String, String)] = Seq.empty
-    ) {
-      val queryComponents = for {
-        (name, values) <- q
-        value <- values
-      } yield name -> value
-      val url = s"${apiUrl}${path}${queryComponents.mkString("?", "&", "")}"
-      apiToken.fold(logger.info(s"curl -X $method $url")) { _ =>
-        logger.info(s"curl -X $method -u '[REDACTED]:' $url")
+    def incidents: Incidents = Incidents
+
+    def _requestBuilder(method: String, path: String): RequestBuilder = {
+      val builder = new RequestBuilder(method)
+        .setUrl(apiUrl + path)
+        .addHeader("User-Agent", UserAgent)
+
+      apiToken.fold(builder) { token =>
+        builder.setRealm(
+          new Realm.RealmBuilder()
+            .setPrincipal(token)
+            .setScheme(Realm.AuthScheme.BASIC)
+            .build()
+        )
       }
     }
 
-    def GET(
+    def _logRequest(request: Request) {
+      println("_logRequest: " + request)
+    }
+
+    def _executeRequest(
+      method: String,
       path: String,
-      q: Seq[(String, String)] = Seq.empty
+      queryParameters: Seq[(String, String)] = Seq.empty,
+      formParameters: Seq[(String, String)] = Seq.empty,
+      body: Option[play.api.libs.json.JsValue] = None
     )(implicit ec: scala.concurrent.ExecutionContext): scala.concurrent.Future[Response] = {
-      val url = apiUrl + path
-      _logRequest("GET", url, q)
-      // TODO: Query parameters
+      val request = _requestBuilder(method, path)
+
+      queryParameters.foreach { pair =>
+        request.addQueryParameter(pair._1, pair._2)
+      }
+
+      queryParameters.foreach { pair =>
+        request.addParameter(pair._1, pair._2)
+      }
+
+      val requestWithParamsAndBody = body.fold(request) { b =>
+        val serialized = play.api.libs.json.Json.stringify(b)
+        request.setBody(serialized).addHeader("Content-type", "application/json")
+      }
+
+      val finalRequest = requestWithParamsAndBody.build()
+      _logRequest(finalRequest)
 
       val result = Promise[Response]()
-      asyncHttpClient.prepareGet(url).execute(
+      asyncHttpClient.executeRequest(finalRequest,
         new AsyncCompletionHandler[Unit]() {
           override def onCompleted(r: Response) = result.success(r)
         }
@@ -67,17 +90,15 @@ object Foo {
     }
 
     object Healthchecks extends Healthchecks {
-      import com.gilt.quality.models.json.jsonReadsQualityHealthcheck
-
       override def get()(implicit ec: scala.concurrent.ExecutionContext):
           scala.concurrent.Future[scala.Option[com.gilt.quality.models.Healthcheck]] =
       {
-        GET("/_internal_/healthcheck").map {
+        _executeRequest("GET", "/_internal_/healthcheck").map {
           case r if r.getStatusCode == 200 => {
             play.api.libs.json.Json.parse(r.getResponseBody("UTF-8")).validate[com.gilt.quality.models.Healthcheck] match {
               case play.api.libs.json.JsSuccess(x, _) => Some(x)
               case play.api.libs.json.JsError(errors) => {
-                throw new FailedRequest(r, Some("Invalid json for com.gilt.quality.models.Healthcheck: " + errors.mkString(" ")))
+                throw new FailedRequest(r, Some("Invalid json: " + errors.mkString(" ")))
               }
             }
           }
@@ -86,6 +107,173 @@ object Foo {
         }
       }
     }
+
+    object Incidents extends Incidents {
+      override def get(
+        id: scala.Option[Long] = None,
+        teamKey: scala.Option[String] = None,
+        hasTeam: scala.Option[Boolean] = None,
+        hasPlan: scala.Option[Boolean] = None,
+        hasGrade: scala.Option[Boolean] = None,
+        limit: scala.Option[Int] = None,
+        offset: scala.Option[Int] = None
+      )(implicit ec: scala.concurrent.ExecutionContext): scala.concurrent.Future[scala.collection.Seq[com.gilt.quality.models.Incident]] = {
+        val query = Seq(
+          id.map("id" -> _.toString),
+          teamKey.map("team_key" -> _),
+          hasTeam.map("has_team" -> _.toString),
+          hasPlan.map("has_plan" -> _.toString),
+          hasGrade.map("has_grade" -> _.toString),
+          limit.map("limit" -> _.toString),
+          offset.map("offset" -> _.toString)
+        ).flatten
+
+        _executeRequest("GET", s"/incidents", query).map {
+          case r if r.getStatusCode == 200 => {
+            play.api.libs.json.Json.parse(r.getResponseBody("UTF-8")).validate[scala.collection.Seq[com.gilt.quality.models.Incident]] match {
+              case play.api.libs.json.JsSuccess(x, _) => x
+              case play.api.libs.json.JsError(errors) => {
+                throw new FailedRequest(r, Some("Invalid json: " + errors.mkString(" ")))
+              }
+            }
+          }
+          case r => throw new FailedRequest(r)
+        }
+      }
+
+      override def getById(
+        id: Long
+      )(implicit ec: scala.concurrent.ExecutionContext): scala.concurrent.Future[scala.Option[com.gilt.quality.models.Incident]] = {
+        _executeRequest("GET", s"/incidents/${id}").map {
+          case r if r.getStatusCode == 200 => {
+            play.api.libs.json.Json.parse(r.getResponseBody("UTF-8")).validate[com.gilt.quality.models.Incident] match {
+              case play.api.libs.json.JsSuccess(x, _) => Some(x)
+              case play.api.libs.json.JsError(errors) => {
+                throw new FailedRequest(r, Some("Invalid json: " + errors.mkString(" ")))
+              }
+            }
+          }
+          case r if r.getStatusCode == 404 => None
+          case r => throw new FailedRequest(r)
+        }
+      }
+
+      override def post(
+        teamKey: scala.Option[String] = None,
+        severity: String,
+        summary: String,
+        description: scala.Option[String] = None,
+        tags: scala.collection.Seq[String] = Nil
+      )(implicit ec: scala.concurrent.ExecutionContext): scala.concurrent.Future[com.gilt.quality.models.Incident] = {
+        val payload = play.api.libs.json.Json.obj(
+          "team_key" -> play.api.libs.json.Json.toJson(teamKey),
+          "severity" -> play.api.libs.json.Json.toJson(severity),
+          "summary" -> play.api.libs.json.Json.toJson(summary),
+          "description" -> play.api.libs.json.Json.toJson(description),
+          "tags" -> play.api.libs.json.Json.toJson(tags)
+        )
+
+        _executeRequest("POST", s"/incidents", body = Some(payload)).map {
+          case r if r.getStatusCode == 201 => {
+            play.api.libs.json.Json.parse(r.getResponseBody("UTF-8")).validate[com.gilt.quality.models.Incident] match {
+              case play.api.libs.json.JsSuccess(x, _) => x
+              case play.api.libs.json.JsError(errors) => {
+                throw new FailedRequest(r, Some("Invalid json: " + errors.mkString(" ")))
+              }
+            }
+          }
+          // TODO case r if r.getStatusCode == 409 => throw new com.gilt.quality.error.ErrorsResponse(r)
+          case r => throw new FailedRequest(r)
+        }
+      }
+
+/*
+      override def putById(
+        id: Long,
+        teamKey: scala.Option[String] = None,
+        severity: String,
+        summary: String,
+        description: scala.Option[String] = None,
+        tags: scala.collection.Seq[String] = Nil
+      )(implicit ec: scala.concurrent.ExecutionContext): scala.concurrent.Future[com.gilt.quality.models.Incident] = {
+        val payload = play.api.libs.json.Json.obj(
+          "team_key" -> play.api.libs.json.Json.toJson(teamKey),
+          "severity" -> play.api.libs.json.Json.toJson(severity),
+          "summary" -> play.api.libs.json.Json.toJson(summary),
+          "description" -> play.api.libs.json.Json.toJson(description),
+          "tags" -> play.api.libs.json.Json.toJson(tags)
+        )
+
+        PUT(s"/incidents/${id}", payload).map {
+          case r if r.getStatusCode == 201 => r.json.as[com.gilt.quality.models.Incident]
+          case r if r.getStatusCode == 409 => throw new com.gilt.quality.error.ErrorsResponse(r)
+          case r => throw new FailedRequest(r)
+        }
+      }
+
+      override def deleteById(
+        id: Long
+      )(implicit ec: scala.concurrent.ExecutionContext): scala.concurrent.Future[scala.Option[Unit]] = {
+        DELETE(s"/incidents/${id}").map {
+          case r if r.getStatusCode == 204 => Some(Unit)
+          case r if r.getStatusCode == 404 => None
+          case r => throw new FailedRequest(r)
+        }
+      }
+ */
+    }
+
+    trait Incidents {
+      /**
+       * Search all incidents. Results are always paginated.
+       */
+      def get(
+        id: scala.Option[Long] = None,
+        teamKey: scala.Option[String] = None,
+        hasTeam: scala.Option[Boolean] = None,
+        hasPlan: scala.Option[Boolean] = None,
+        hasGrade: scala.Option[Boolean] = None,
+        limit: scala.Option[Int] = None,
+        offset: scala.Option[Int] = None
+      )(implicit ec: scala.concurrent.ExecutionContext): scala.concurrent.Future[scala.collection.Seq[com.gilt.quality.models.Incident]]
+
+      /**
+       * Returns information about the incident with this specific id.
+       */
+      def getById(
+        id: Long
+      )(implicit ec: scala.concurrent.ExecutionContext): scala.concurrent.Future[scala.Option[com.gilt.quality.models.Incident]]
+
+      /**
+       * Create a new incident.
+       */
+      def post(
+        teamKey: scala.Option[String] = None,
+        severity: String,
+        summary: String,
+        description: scala.Option[String] = None,
+        tags: scala.collection.Seq[String] = Nil
+      )(implicit ec: scala.concurrent.ExecutionContext): scala.concurrent.Future[com.gilt.quality.models.Incident]
+
+      /**
+       * Updates an incident.
+       */
+/*
+      def putById(
+        id: Long,
+        teamKey: scala.Option[String] = None,
+        severity: String,
+        summary: String,
+        description: scala.Option[String] = None,
+        tags: scala.collection.Seq[String] = Nil
+      )(implicit ec: scala.concurrent.ExecutionContext): scala.concurrent.Future[com.gilt.quality.models.Incident]
+
+      def deleteById(
+        id: Long
+      )(implicit ec: scala.concurrent.ExecutionContext): scala.concurrent.Future[scala.Option[Unit]]
+ */
+    }
+
   }
 
   case class FailedRequest(
