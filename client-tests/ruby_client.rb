e0f5b4b47ba330fd2fa97c3ba504544e73a5ef59
Michael Bryzek <michael@gilt.com>
2014-10-21 23:17:17 -0400

    Update to latest client (HEAD support)

--- client-tests/ruby_client.rb
+++ client-tests/ruby_client.rb
@@ -14,7 +14,7 @@ module Apidoc
 
   class Client
 
-    USER_AGENT = 'apidoc:0.6.8 http://www.apidoc.me/gilt/code/apidoc/0.6.6/ruby_client' unless defined?(USER_AGENT)
+    USER_AGENT = 'apidoc:0.6.9 http://www.apidoc.me/gilt/code/apidoc/0.6.9/ruby_client' unless defined?(USER_AGENT)
 
     def initialize(url, opts={})
       @url = HttpClient::Preconditions.assert_class('url', url, String)
--- generated/app/GeneratedClient.scala
+++ generated/app/GeneratedClient.scala
@@ -399,7 +399,7 @@ package com.gilt.apidoc {
   class Client(apiUrl: String, apiToken: scala.Option[String] = None) {
     import com.gilt.apidoc.models.json._
 
-    private val UserAgent = "apidoc:0.6.8 http://www.apidoc.me/gilt/code/apidoc/0.6.6/play_2_3_client"
+    private val UserAgent = "apidoc:0.6.9 http://www.apidoc.me/gilt/code/apidoc/0.6.9/play_2_3_client"
     private val logger = play.api.Logger("com.gilt.apidoc.client")
 
     logger.info(s"Initializing com.gilt.apidoc.client for url $apiUrl")
@@ -917,6 +917,9 @@ package com.gilt.apidoc {
         }
         case "DELETE" => {
           _logRequest("DELETE", _requestHolder(path).withQueryString(queryParameters:_*)).delete()
+        }
+         case "HEAD" => {
+          _logRequest("HEAD", _requestHolder(path).withQueryString(queryParameters:_*)).head()
         }
         case _ => {
           _logRequest(method, _requestHolder(path).withQueryString(queryParameters:_*))
