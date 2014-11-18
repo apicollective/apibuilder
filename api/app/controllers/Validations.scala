cdfb022ce75459ff90d3c06ad17c2de9dbda6928
Michael Bryzek <michael@gilt.com>
2014-11-18 09:50:17 -0500

    Replace ValidationResult with Validation from generated client

--- api/app/controllers/Validations.scala
+++ api/app/controllers/Validations.scala
@@ -1,22 +1,17 @@
 package controllers
 
+import com.gilt.apidoc.models.Validation
 import core.ServiceDescriptionValidator
 import play.api.mvc._
 import play.api.libs.json._
 
 object Validations extends Controller {
 
-  case class ValidationResult(valid: Boolean, errors: Seq[String])
-
-  object ValidationResult {
-    implicit val validationResultWrites = Json.writes[ValidationResult]
-  }
-
   def post() = Action(parse.temporaryFile) { request =>
     val contents = scala.io.Source.fromFile(request.body.file).getLines.mkString("\n")
     ServiceDescriptionValidator(contents).errors match {
-      case Nil => Ok(Json.toJson(ValidationResult(true, Nil)))
-      case errors => Ok(Json.toJson(ValidationResult(false, errors)))
+      case Nil => Ok(Json.toJson(Validation(true, Nil)))
+      case errors => Ok(Json.toJson(Validation(false, errors)))
     }
   }
 
