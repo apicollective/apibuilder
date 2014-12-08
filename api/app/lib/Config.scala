255b2d7992f52186c0e0f1c500508d61f8ef03d7
Michael Bryzek <michael@gilt.com>
2014-12-08 15:32:29 +0000

    Better error message on config error

--- api/app/lib/Config.scala
+++ api/app/lib/Config.scala
@@ -1,5 +1,6 @@
 package lib
 
+import play.api.Logger
 import play.api.Play.current
 
 /**
@@ -10,13 +11,17 @@ object Config {
 
   def requiredString(name: String): String = {
     optionalString(name).getOrElse {
+      val msg = s"configuration parameter[$name] is required"
+      Logger.error(msg)
       sys.error(s"configuration parameter[$name] is required")
     }
   }
 
   def optionalString(name: String): Option[String] = {
     current.configuration.getString(name).map { value =>
-      assert(value.trim != "", "Value for configuration parameter[$name] cannot be blank")
+      val msg = s"Value for configuration parameter[$name] cannot be blank"
+      Logger.error(msg)
+      assert(value.trim != "", msg)
       value
     }
   }
