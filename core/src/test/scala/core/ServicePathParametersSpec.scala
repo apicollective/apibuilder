fb281ed25e29d6309bab08c2a9c868bc942c6c6a
Michael Bryzek <michael@gilt.com>
2015-02-03 11:56:27 -0500

    Add (failing) test case for inferring path parameter type for union types

--- core/src/test/scala/core/ServicePathParametersSpec.scala
+++ core/src/test/scala/core/ServicePathParametersSpec.scala
@@ -5,7 +5,8 @@ import org.scalatest.Matchers
 
 class ServicePathParametersSpec extends FunSpec with Matchers {
 
-  val baseJson = """
+  describe("with a service") {
+    val baseJson = """
     {
       "base_url": "http://localhost:9000",
       "name": "Api Doc",
@@ -50,46 +51,111 @@ class ServicePathParametersSpec extends FunSpec with Matchers {
         }
       }
     }
-  """
+    """
 
-  it("numbers can be path parameters") {
-    val json = baseJson.format("GET", "/:id")
-    ServiceValidator(TestHelper.serviceConfig, json).errors.mkString("") should be("")
+    it("numbers can be path parameters") {
+      val json = baseJson.format("GET", "/:id")
+      ServiceValidator(TestHelper.serviceConfig, json).errors.mkString("") should be("")
+    }
+
+    it("strings can be path parameters") {
+      val json = baseJson.format("GET", "/:name")
+      ServiceValidator(TestHelper.serviceConfig, json).errors.mkString("") should be("")
+    }
+
+    it("parameters not defined on the model are accepted (assumed strings)") {
+      val json = baseJson.format("GET", "/:some_string")
+      ServiceValidator(TestHelper.serviceConfig, json).errors.mkString("") should be("")
+    }
+
+    it("enums can be path parameters - assumed type is string") {
+      val json = baseJson.format("GET", "/:age_group")
+      ServiceValidator(TestHelper.serviceConfig, json).errors.mkString("") should be("")
+    }
+
+    it("dates can be path parameters") {
+      val json = baseJson.format("GET", "/:created_at_date")
+      ServiceValidator(TestHelper.serviceConfig, json).errors.mkString("") should be("")
+    }
+
+    it("date-time can be path parameters") {
+      val json = baseJson.format("GET", "/:created_at_date_time")
+      ServiceValidator(TestHelper.serviceConfig, json).errors.mkString("") should be("")
+    }
+
+    it("other models cannot be path parameters") {
+      val json = baseJson.format("GET", "/:tag")
+      ServiceValidator(TestHelper.serviceConfig, json).errors.mkString("") should be("Resource[user] GET path parameter[tag] has an invalid type[tag]. Valid types for path parameters are: boolean, decimal, integer, double, long, string, date-iso8601, date-time-iso8601, uuid")
+    }
+
+    it("unsupported types declared as parameters are validated") {
+      val json = baseJson.format("POST", "/:tags")
+      ServiceValidator(TestHelper.serviceConfig, json).errors.mkString("") should be("Resource[user] POST path parameter[tags] has an invalid type[map]. Valid types for path parameters are: boolean, decimal, integer, double, long, string, date-iso8601, date-time-iso8601, uuid")
+    }
+
   }
 
-  it("strings can be path parameters") {
-    val json = baseJson.format("GET", "/:name")
-    ServiceValidator(TestHelper.serviceConfig, json).errors.mkString("") should be("")
-  }
+  describe("w/ a union resource") {
 
-  it("parameters not defined on the model are accepted (assumed strings)") {
-    val json = baseJson.format("GET", "/:some_string")
-    ServiceValidator(TestHelper.serviceConfig, json).errors.mkString("") should be("")
-  }
+    val baseJson = s"""
+    {
+      "name": "Api Doc",
+      "unions": {
+        "user": {
+          "types": [
+            { "type": "registered" },
+            { "type": "guest" }
+          ]
+        }
+      },
 
-  it("enums can be path parameters - assumed type is string") {
-    val json = baseJson.format("GET", "/:age_group")
-    ServiceValidator(TestHelper.serviceConfig, json).errors.mkString("") should be("")
-  }
+      "models": {
+        "registered": {
+          "fields": [
+            { "name": "guid", "type": "uuid" },
+            { "name": "age", "type": "integer" }
+          ]
+        },
+        "guest": {
+          "fields": [
+            { "name": "guid", "type": "uuid" },
+            { "name": "age", "type": "long" }
+          ]
+        }
+      },
 
-  it("dates can be path parameters") {
-    val json = baseJson.format("GET", "/:created_at_date")
-    ServiceValidator(TestHelper.serviceConfig, json).errors.mkString("") should be("")
-  }
+      "resources": {
+        "user": {
+          "operations": [
+            { "method": "GET", "path": "/users/:%s" }
+          ]
+        }
+      }
+    }
+    """
 
-  it("date-time can be path parameters") {
-    val json = baseJson.format("GET", "/:created_at_date_time")
-    ServiceValidator(TestHelper.serviceConfig, json).errors.mkString("") should be("")
-  }
+    it("can identify common type for path parameter if all union types have the same type") {
+      val json = baseJson.format("guid")
+      val validator = ServiceValidator(TestHelper.serviceConfig, json)
+      validator.errors.mkString("") should be("")
+      val userResource = validator.service.get.resources.head
+      val op = userResource.operations.head
+      val param = op.parameters.head
+      param.name should be("guid")
+      param.`type` should be("uuid")
+    }
 
-  it("other models cannot be path parameters") {
-    val json = baseJson.format("GET", "/:tag")
-    ServiceValidator(TestHelper.serviceConfig, json).errors.mkString("") should be("Resource[user] GET path parameter[tag] has an invalid type[tag]. Valid types for path parameters are: boolean, decimal, integer, double, long, string, date-iso8601, date-time-iso8601, uuid")
-  }
+    it("uses default 'string' if path parameter type varies across union type") {
+      val json = baseJson.format("age")
+      val validator = ServiceValidator(TestHelper.serviceConfig, json)
+      validator.errors.mkString("") should be("")
+      val userResource = validator.service.get.resources.head
+      val op = userResource.operations.head
+      val param = op.parameters.head
+      param.name should be("age")
+      param.`type` should be("string")
+    }
 
-  it("unsupported types declared as parameters are validated") {
-    val json = baseJson.format("POST", "/:tags")
-    ServiceValidator(TestHelper.serviceConfig, json).errors.mkString("") should be("Resource[user] POST path parameter[tags] has an invalid type[map]. Valid types for path parameters are: boolean, decimal, integer, double, long, string, date-iso8601, date-time-iso8601, uuid")
   }
 
 }
