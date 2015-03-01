91f42c8bcb5684c6137708303f58669820cc69a4
Michael Bryzek <michael@gilt.com>
2015-03-01 11:14:57 -0500

    Consolidate test methods

--- api/test/db/VersionsDaoSpec.scala
+++ api/test/db/VersionsDaoSpec.scala
@@ -11,24 +11,22 @@ class VersionsDaoSpec extends FunSpec with Matchers {
 
   new play.core.StaticApplication(new java.io.File("."))
 
-  val applicationKey = "test-" + UUID.randomUUID.toString
+  private val OriginalJson = Json.obj("name" -> UUID.randomUUID.toString)
 
-  private lazy val application = {
+  private def createApplication(key: String = "test-" + UUID.randomUUID.toString): com.gilt.apidoc.v0.models.Application = {
     val applicationForm = ApplicationForm(
-      name = applicationKey,
-      key = Some(applicationKey),
+      name = key,
+      key = Some(key),
       description = None,
       visibility = Visibility.Organization
     )
     ApplicationsDao.create(Util.createdBy, Util.testOrg, applicationForm)
   }
 
-  private val OriginalJson = Json.obj("name" -> UUID.randomUUID.toString)
-
-  private lazy val service = Service(
-    name = applicationKey,
+  private def createService(app: com.gilt.apidoc.v0.models.Application): Service = Service(
+    name = app.name,
     organization = Organization(key = "test"),
-    application = Application(key = applicationKey),
+    application = Application(key = app.key),
     namespace = "test." + key,
     version = "0.0.1-dev",
     headers = Nil,
@@ -39,23 +37,43 @@ class VersionsDaoSpec extends FunSpec with Matchers {
     resources = Nil
   )
 
-  it("create") {
-    val version = VersionsDao.create(Util.createdBy, application, "1.0.0", OriginalJson, service)
-    version.version should be("1.0.0")
+  describe("with an application") {
+
+    val applicationKey = "test-" + UUID.randomUUID.toString
+    val application: com.gilt.apidoc.v0.models.Application = createApplication(applicationKey)
+    val service = createService(application)
+
+    it("create") {
+      val version = VersionsDao.create(Util.createdBy, application, "1.0.0", OriginalJson, service)
+      version.version should be("1.0.0")
+    }
+
+    it("findByApplicationAndVersion") {
+      VersionsDao.create(Util.createdBy, application, "1.0.1", OriginalJson, service)
+      VersionsDao.findByApplicationAndVersion(Authorization.All, application, "1.0.1").map(_.service) should be(Some(service))
+    }
+
+    it("soft delete") {
+      val version1 = VersionsDao.create(Util.createdBy, application, "1.0.2", OriginalJson, service)
+      VersionsDao.softDelete(Util.createdBy, version1)
+
+      val version2 = VersionsDao.create(Util.createdBy, application, "1.0.2", OriginalJson, service)
+      version2.copy(guid = version1.guid) should be(version1)
+      version2.guid shouldNot be(version1.guid)
+    }
+
   }
 
-  it("findByApplicationAndVersion") {
-    VersionsDao.create(Util.createdBy, application, "1.0.1", OriginalJson, service)
-    VersionsDao.findByApplicationAndVersion(Authorization.All, application, "1.0.1").map(_.service) should be(Some(service))
-  }
+  it("sorts properly") {
+    val app = createApplication()
+    val service = createService(app)
+    val version1 = VersionsDao.create(Util.createdBy, app, "1.0.2", OriginalJson, service)
+    val version2 = VersionsDao.create(Util.createdBy, app, "1.0.2-dev", OriginalJson, service)
 
-  it("soft delete") {
-    val version1 = VersionsDao.create(Util.createdBy, application, "1.0.2", OriginalJson, service)
-    VersionsDao.softDelete(Util.createdBy, version1)
-
-    val version2 = VersionsDao.create(Util.createdBy, application, "1.0.2", OriginalJson, service)
-    version2.copy(guid = version1.guid) should be(version1)
-    version2.guid shouldNot be(version1.guid)
+    VersionsDao.findAll(
+      Authorization.All,
+      applicationGuid = Some(app.guid)
+    ).map(_.version) should be(Seq("1.0.2", "1.0.2-dev"))
   }
 
 }
