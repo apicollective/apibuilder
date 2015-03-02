111a64bb27028fc07ad5ffd2d9db233bd24da97b
Michael Bryzek <michael@gilt.com>
2015-03-02 18:35:57 -0500

    Add deprecation to Enum

--- core/src/test/scala/core/DeprecationSpec.scala
+++ core/src/test/scala/core/DeprecationSpec.scala
@@ -4,6 +4,37 @@ import org.scalatest.{FunSpec, Matchers}
 
 class DeprecationSpec extends FunSpec with Matchers {
 
+  it("enum") {
+    val json = """
+    {
+      "name": "Api Doc",
+
+      "enums": {
+        "old_content_type": {
+          "deprecation": { "description": "blah" },
+          "values": [
+            { "name": "application/json" },
+            { "name": "application/xml" }
+          ]
+        },
+
+        "content_type": {
+          "values": [
+            { "name": "application/json" },
+            { "name": "application/xml" }
+          ]
+        }
+      }
+    }
+    """
+
+    val validator = ServiceValidator(TestHelper.serviceConfig, json)
+    validator.errors.mkString("") should be("")
+    validator.service.get.enums.find(_.name == "old_content_type").get.deprecation.flatMap(_.description) should be(Some("blah"))
+    validator.service.get.enums.find(_.name == "content_type").get.deprecation.flatMap(_.description) should be(None)
+  }
+
+
   it("enum value") {
     val json = """
     {
