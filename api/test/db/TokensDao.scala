40a689ed340771cdc1c8b3e909054f79210ddd34
Michael Bryzek <michael@gilt.com>
2015-03-16 09:25:14 -0400

    Next attempt at fixing test case across JDK 7/8

--- api/test/db/TokensDao.scala
+++ api/test/db/TokensDao.scala
@@ -5,8 +5,8 @@ import org.scalatest.{FunSpec, Matchers}
 class TokensDaoSpec extends FunSpec with Matchers {
 
   it("obfuscate") {
-    TokensDao.obfuscate("") should be("XXXX-XXXX-XXXX")
-    TokensDao.obfuscate("123lkadfslkj34j123l4kabcde") should be("123-XXXX-bcde")
+    TokensDao.obfuscate("") should be("XXX-XXXX-XXXX")
+    TokensDao.obfuscate("123lkadfslkj34j123l4kabcde") should be("XXX-XXXX-bcde")
   }
 
 }
