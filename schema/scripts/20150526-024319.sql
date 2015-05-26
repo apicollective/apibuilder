281bc67e18422e52fdf0a37552cbfe7998009f71
Michael Bryzek <michael@gilt.com>
2015-05-26 15:29:43 +0100

    Fix missing clause on unique index on generators(key)

--- schema/scripts/20150526-024319.sql
+++ schema/scripts/20150526-024319.sql
@@ -1,2 +1,2 @@
 alter table generators alter column uri set not null;
-create unique index generators_key_not_deleted_un_idx on generators(key);
+create unique index generators_key_not_deleted_un_idx on generators(key) where deleted_at is null;
