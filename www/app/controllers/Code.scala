02599905f77913688e8b1c75bfba0efb4fac9d4a
Michael Bryzek <mbryzek@alum.mit.edu>
2015-07-13 17:49:48 +0200

    Update UI to list all of the file names that are part of a generator
     - Through UI, the user will now download each individual file

--- www/app/controllers/Code.scala
+++ www/app/controllers/Code.scala
@@ -7,12 +7,37 @@ import play.api.mvc._
 
 object Code extends Controller {
 
-  def generate(orgKey: String, applicationKey: String, version: String, generatorKey: String) = AnonymousOrg.async { request =>
+  def index(orgKey: String, applicationKey: String, version: String, generatorKey: String) = AnonymousOrg.async { implicit request =>
     lib.ApiClient.callWith404(
       request.api.Code.getByOrgKeyAndApplicationKeyAndVersionAndGeneratorKey(orgKey, applicationKey, version, generatorKey)
     ).map {
       case None => Redirect(routes.Versions.show(orgKey, applicationKey, version)).flashing("warning" -> "Version not found")
-      case Some(r) => Ok(r.source)
+      case Some(code) => {
+        Ok(views.html.code.index(
+          request.mainTemplate().copy(title = Some(code.generator.name + " - Files")),
+          files = code.files
+        ))
+      }
+    }.recover {
+      case r: com.bryzek.apidoc.api.v0.errors.ErrorsResponse => {
+        Redirect(routes.Versions.show(orgKey, applicationKey, version)).flashing("warning" -> r.errors.map(_.message).mkString(", "))
+      }
+    }
+  }
+
+  def file(orgKey: String, applicationKey: String, version: String, generatorKey: String, fileName: String) = AnonymousOrg.async { implicit request =>
+    lib.ApiClient.callWith404(
+      request.api.Code.getByOrgKeyAndApplicationKeyAndVersionAndGeneratorKey(orgKey, applicationKey, version, generatorKey)
+    ).map {
+      case None => Redirect(routes.Versions.show(orgKey, applicationKey, version)).flashing("warning" -> "Version not found")
+      case Some(code) => {
+        code.files.find(_.name == fileName) match {
+          case None => Redirect(routes.Versions.show(orgKey, applicationKey, version)).flashing("warning" -> s"File $fileName not found")
+          case Some(file) => {
+            Ok(file.contents)
+          }
+        }
+      }
     }.recover {
       case r: com.bryzek.apidoc.api.v0.errors.ErrorsResponse => {
         Redirect(routes.Versions.show(orgKey, applicationKey, version)).flashing("warning" -> r.errors.map(_.message).mkString(", "))
