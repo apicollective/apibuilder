3f6c2064071ba7b1114d57981bd9f2dafe591004
Michael Bryzek <mbryzek@alum.mit.edu>
2015-07-19 13:27:31 -0400

    Move test application to standalone file

--- api/test/db/ItemsDaoSpec.scala
+++ api/test/db/ItemsDaoSpec.scala
@@ -5,16 +5,7 @@ import org.scalatest.{FunSpec, Matchers}
 import org.postgresql.util.PSQLException
 import java.util.UUID
 
-class ItemsDaoSpec extends FunSpec with Matchers {
-
-  // new play.core.StaticApplication(new java.io.File("."))
-  import play.api._
-  val env = Environment(new java.io.File("."), this.getClass.getClassLoader, Mode.Dev)
-  val context = ApplicationLoader.createContext(env)
-  val loader = ApplicationLoader(context)
-  val app = loader.load(context)
-  Play.start(app)
-  import Play.current
+class ItemsDaoSpec extends FunSpec with Matchers with util.TestApplication {
 
   private[this] def upsertItem(
     org: Organization = Util.createOrganization(),
