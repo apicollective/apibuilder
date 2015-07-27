bd0cc8bdd5f36b8b4c103c2f1e99538442d9b3ff
Michael Bryzek <mbryzek@alum.mit.edu>
2015-07-27 12:04:42 -0400

    Add explicit route for zip file (fixes nav bar link target)

--- www/app/controllers/Code.scala
+++ www/app/controllers/Code.scala
@@ -13,17 +13,26 @@ class Code @Inject() (val messagesApi: MessagesApi) extends Controller with I18n
     ).map {
       case None => Redirect(routes.Versions.show(orgKey, applicationKey, version)).flashing("warning" -> "Version not found")
       case Some(code) => {
+        Ok(views.html.code.index(
+          request.mainTemplate().copy(title = Some(code.generator.name + " - Files")),
+          files = code.files
+        ))
+      }
+    }.recover {
+      case r: com.bryzek.apidoc.api.v0.errors.ErrorsResponse => {
+        Redirect(routes.Versions.show(orgKey, applicationKey, version)).flashing("warning" -> r.errors.map(_.message).mkString(", "))
+      }
+    }
+  }
 
-        // TODO: Better content negotiation here
-        if (request.path.endsWith(".zip")) {
-          val zipFile = lib.Zipfile.create(s"${orgKey}_${applicationKey}_${version}", code.files)
-          Ok.sendFile(zipFile)
-        } else {
-          Ok(views.html.code.index(
-            request.mainTemplate().copy(title = Some(code.generator.name + " - Files")),
-            files = code.files
-          ))
-        }
+  def zipFile(orgKey: String, applicationKey: String, version: String, generatorKey: String) = AnonymousOrg.async { implicit request =>
+    lib.ApiClient.callWith404(
+      request.api.Code.getByOrgKeyAndApplicationKeyAndVersionAndGeneratorKey(orgKey, applicationKey, version, generatorKey)
+    ).map {
+      case None => Redirect(routes.Versions.show(orgKey, applicationKey, version)).flashing("warning" -> "Version not found")
+      case Some(code) => {
+        val zipFile = lib.Zipfile.create(s"${orgKey}_${applicationKey}_${version}", code.files)
+        Ok.sendFile(zipFile)
       }
     }.recover {
       case r: com.bryzek.apidoc.api.v0.errors.ErrorsResponse => {
