641b489a64bd801bdf78fcbb351723f29b02b3d3
Michael Bryzek <mbryzek@alum.mit.edu>
2016-02-26 08:47:02 -0500

    Remove datatype. Make list, map proper types

---
+++ lib/src/test/scala/KindSpec.scala
@@ -0,0 +1,68 @@
+package lib
+
+import org.scalatest.{FunSpec, Matchers}
+
+class KindSpec extends FunSpec with Matchers {
+
+  it("primitives") {
+    val resolver = DatatypeResolver(
+      enumNames = Seq.empty,
+      modelNames = Seq.empty,
+      unionNames = Seq.empty
+    )
+
+    resolver.parse("string").map(_.toString) should be(Some("string"))
+    resolver.parse("long").map(_.toString) should be(Some("long"))
+    resolver.parse("uuid").map(_.toString) should be(Some("uuid"))
+    resolver.parse("unit").map(_.toString) should be(Some("unit"))
+    resolver.parse("integer").map(_.toString) should be(Some("integer"))
+    resolver.parse("date-time-iso8601").map(_.toString) should be(Some("date-time-iso8601"))
+
+    resolver.parse("[string]").map(_.toString) should be(Some("[string]"))
+    resolver.parse("[long]").map(_.toString) should be(Some("[long]"))
+    resolver.parse("[uuid]").map(_.toString) should be(Some("[uuid]"))
+    resolver.parse("[unit]").map(_.toString) should be(Some("[unit]"))
+    resolver.parse("[integer]").map(_.toString) should be(Some("[integer]"))
+    resolver.parse("[date-time-iso8601]").map(_.toString) should be(Some("[date-time-iso8601]"))
+
+    resolver.parse("map").map(_.toString) should be(Some("map[string]"))
+    resolver.parse("map[string]").map(_.toString) should be(Some("map[string]"))
+    resolver.parse("map[long]").map(_.toString) should be(Some("map[long]"))
+    resolver.parse("map[uuid]").map(_.toString) should be(Some("map[uuid]"))
+    resolver.parse("map[unit]").map(_.toString) should be(Some("map[unit]"))
+    resolver.parse("map[integer]").map(_.toString) should be(Some("map[integer]"))
+    resolver.parse("map[date-time-iso8601]").map(_.toString) should be(Some("map[date-time-iso8601]"))
+
+    resolver.parse("user") should be(None)
+  }
+
+  it("with enums and models") {
+    val resolver = DatatypeResolver(
+      enumNames = Seq("age_group"),
+      modelNames = Seq("user"),
+      unionNames = Seq.empty
+    )
+
+    resolver.parse("map[age_group]").map(_.toString) should be(Some("map[enum[age_group]]"))
+    resolver.parse("user").map(_.toString) should be(Some("model[user]"))
+
+    resolver.parse("group").map(_.toString) should be(None)
+  }
+
+  it("precedence rules") {
+    val resolver = DatatypeResolver(
+      enumNames = Seq("age_group", "string", "other_enum"),
+      modelNames = Seq("guest_user", "registered_user", "uuid"),
+      unionNames = Seq("user", "other_enum")
+    )
+
+    resolver.parse("age_group") should be(Some(Kind.Enum("age_group")))
+    resolver.parse("other_enum") should be(Some(Kind.Enum("other_enum")))
+    resolver.parse("string") should be(Some(Kind.Primitive("string")))
+    resolver.parse("guest_user") should be(Some(Kind.Model("guest_user")))
+    resolver.parse("registered_user") should be(Some(Kind.Model("registered_user")))
+    resolver.parse("uuid") should be(Some(Kind.Primitive("uuid")))
+    resolver.parse("user") should be(Some(Kind.Union("user")))
+  }
+
+}
