a3c90bc5226b4bee0f2549e132ad10c4ee487c61
Michael Bryzek <mbryzek@alum.mit.edu>
2016-02-26 16:14:31 -0500

    Make zip file look like tarfile

--- www/app/controllers/Code.scala
+++ www/app/controllers/Code.scala
@@ -1,9 +1,11 @@
 package controllers
 
+import com.bryzek.apidoc.api.v0.Client
+import com.bryzek.apidoc.generator.v0.models.File
 import scala.concurrent.ExecutionContext.Implicits.global
 import javax.inject.Inject
 import play.api.i18n.{MessagesApi, I18nSupport}
-import play.api.mvc.{Action, Controller}
+import play.api.mvc.{Action, Controller, Result}
 
 class Code @Inject() (val messagesApi: MessagesApi) extends Controller with I18nSupport {
 
@@ -29,35 +31,15 @@ class Code @Inject() (val messagesApi: MessagesApi) extends Controller with I18n
     }
   }
 
-  def zipFile(orgKey: String, applicationKey: String, version: String, generatorKey: String) = AnonymousOrg.async { implicit request =>
-    lib.ApiClient.callWith404(
-      request.api.Code.get(orgKey, applicationKey, version, generatorKey)
-    ).map {
-      case None => Redirect(routes.Versions.show(orgKey, applicationKey, version)).flashing("warning" -> "Version not found")
-      case Some(code) => {
-        val zipFile = lib.Zipfile.create(s"${orgKey}_${applicationKey}_${version}", code.files)
-        Ok.sendFile(zipFile)
-      }
-    }.recover {
-      case r: com.bryzek.apidoc.api.v0.errors.ErrorsResponse => {
-        Redirect(routes.Versions.show(orgKey, applicationKey, version)).flashing("warning" -> r.errors.map(_.message).mkString(", "))
-      }
+  def zipFile(orgKey: String, applicationKey: String, version: String, generatorKey: String, fileName: String) = AnonymousOrg.async { implicit request =>
+    generateFile(request.api, orgKey, applicationKey, version, generatorKey) { files =>
+      lib.Zipfile.create(fileName, files)
     }
   }
 
   def tarballFile(orgKey: String, applicationKey: String, version: String, generatorKey: String, fileName: String) = AnonymousOrg.async { implicit request =>
-    lib.ApiClient.callWith404(
-      request.api.Code.get(orgKey, applicationKey, version, generatorKey)
-    ).map {
-      case None => Redirect(routes.Versions.show(orgKey, applicationKey, version)).flashing("warning" -> "Version not found")
-      case Some(code) => {
-        val tarball = lib.TarballFile.create(fileName, code.files)
-        Ok.sendFile(tarball, inline = true)
-      }
-    }.recover {
-      case r: com.bryzek.apidoc.api.v0.errors.ErrorsResponse => {
-        Redirect(routes.Versions.show(orgKey, applicationKey, version)).flashing("warning" -> r.errors.map(_.message).mkString(", "))
-      }
+    generateFile(request.api, orgKey, applicationKey, version, generatorKey) { files =>
+      lib.TarballFile.create(fileName, files)
     }
   }
 
@@ -73,6 +55,29 @@ class Code @Inject() (val messagesApi: MessagesApi) extends Controller with I18n
             Ok(file.contents)
           }
         }
+      }
+    }.recover {
+      case r: com.bryzek.apidoc.api.v0.errors.ErrorsResponse => {
+        Redirect(routes.Versions.show(orgKey, applicationKey, version)).flashing("warning" -> r.errors.map(_.message).mkString(", "))
+      }
+    }
+  }
+
+  private[this] def generateFile(
+    api: Client, orgKey: String, applicationKey: String, version: String, generatorKey: String
+  ) (
+    f: Seq[File] => java.io.File
+  ) = {
+    lib.ApiClient.callWith404(
+      api.Code.get(orgKey, applicationKey, version, generatorKey)
+    ).map {
+      case None => {
+        Redirect(routes.Versions.show(orgKey, applicationKey, version)).flashing("warning" -> "Version not found")
+      }
+
+      case Some(code) => {
+        val result = f(code.files)
+        Ok.sendFile(result, inline = true)
       }
     }.recover {
       case r: com.bryzek.apidoc.api.v0.errors.ErrorsResponse => {
