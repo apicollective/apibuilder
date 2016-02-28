1f4aa9a3868503f245a1dfab8683c6d90b9f50d8
Michael Bryzek <mbryzek@alum.mit.edu>
2016-02-28 09:26:30 -0500

    Work in progress on displaying links to types

--- www/app/lib/TypeLabel.scala
+++ www/app/lib/TypeLabel.scala
@@ -2,6 +2,7 @@ package lib
 
 import com.bryzek.apidoc.api.v0.models.{Application, Organization}
 import com.bryzek.apidoc.spec.v0.models.{Import, Service}
+import play.api.Logger
 
 case class TypeLabel(
   org: Organization,
@@ -11,43 +12,69 @@ case class TypeLabel(
   typeName: String
 ) {
 
-  val link = DatatypeResolver(
+  private[this] val localResolver = DatatypeResolver(
     enumNames = service.enums.map(_.name),
     modelNames = service.models.map(_.name),
     unionNames = service.unions.map(_.name)
-  ).parse(typeName) match {
+  )
+
+  // The last type will be the concrete type. All other types are
+  // wrappers (e.g. Map, List)
+  private[this] val types = TextDatatype.parse(typeName)
+
+  private[this] val typeLink = types.lastOption match {
     case None => {
-      TextDatatype(typeName) match {
-        case TextDatatype.List(t) => resolveImportedType(t) match {
-          case None => typeName
-          case Some(impType) => s"[$impType]"
+      // Unknown type - just display the type name w/out a link
+      typeName
+    }
+
+    case Some(t) => {
+      t match {
+        case TextDatatype.List | TextDatatype.Map => {
+          Logger.error(s"Unexpected list or map type when resolving typeName[$typeName]")
+          typeName
         }
-        case TextDatatype.Map(t) => resolveImportedType(t) match {
-          case None => typeName
-          case Some(impType) => s"map[$impType]"
+        case TextDatatype.Singleton(name) => {
+          localResolver.parse(name).map { kind => Href(org.key, app.key, version, kind).html }.getOrElse {
+            importedTypeLink(name).getOrElse(typeName)
+          }
         }
-        case TextDatatype.Singleton(t) => resolveImportedType(t).getOrElse(typeName)
       }
     }
-    case Some(Datatype.List(t)) => "[" + typeLink(t) + "]"
-    case Some(Datatype.Map(t)) => "map[" + typeLink(t) + "]"
-    case Some(Datatype.Singleton(t)) => typeLink(t)
   }
 
-  private[this] def typeLink(t: Type): String = {
-    Href(org.key, app.key, version, t).html
+  val link: String = buildLabel(typeLink, types.reverse.drop(1))
+
+  @scala.annotation.tailrec
+  private[this] def buildLabel(label: String, types: Seq[TextDatatype]): String = {
+    types match {
+      case Nil => {
+        label
+      }
+      case next :: rest => {
+        next match {
+          case TextDatatype.List => buildLabel(s"[$label]", rest)
+          case TextDatatype.Map => buildLabel(s"map[$label]", rest)
+          case TextDatatype.Singleton(name) => sys.error(s"Unexpected singleton when building link for label[$label] types[$types]")
+        }
+      }
+    }
   }
 
-  private[this] def resolveImportedType(typeName: String): Option[String] = {
+  /**
+    * Looks for the specified type in the imports for this service. If
+    * found, returns a link to that type.
+    */
+  private[this] def importedTypeLink(typeName: String): Option[String] = {
     service.imports.flatMap { imp =>
       imp.enums.find { name => s"${imp.namespace}.enums.$name" == typeName } match {
-        case Some(shortName) => Some(importLink(imp, Kind.Enum, shortName, typeName))
+        case Some(shortName) => Some(importLink(imp, "enum", shortName, typeName))
         case None => {
           imp.models.find { name => s"${imp.namespace}.models.$name" == typeName } match {
-            case Some(shortName) => Some(importLink(imp, Kind.Model, shortName, typeName))
+            case Some(shortName) => Some(importLink(imp, "model", shortName, typeName))
             case None => {
               imp.unions.find { name => s"${imp.namespace}.unions.$name" == typeName } match {
-                case Some(shortName) => Some(importLink(imp, Kind.Union, shortName, typeName))
+                case Some(shortName) => Some(importLink(imp, "union", shortName, typeName))
                 case None => None
               }
             }
@@ -57,9 +84,8 @@ case class TypeLabel(
     }.headOption
   }
 
-  private[this] def importLink(imp: Import, kind: Kind, shortName: String, fullName: String): String = {
-    s"<a href='/${imp.organization.key}/${imp.application.key}/${imp.version}#$kind-$shortName'>$fullName:${imp.version}</a>"
-
+  private[this] def importLink(imp: Import, kind: String, shortName: String, fullName: String): String = {
+    Href(fullName, Href.prefix(imp.organization.key, imp.application.key, imp.version) + s"#$kind-${UrlKey.generate(shortName)}").label
   }
 
 }
@@ -74,19 +100,22 @@ object Href {
     s"/${orgKey}/${appKey}/${version}"
   }
 
-  def apply(orgKey: String, appKey: String, version: String, t: Type): Href = {
-    t match {
-      case Type(Kind.Primitive, name) => {
+  def apply(orgKey: String, appKey: String, version: String, kind: Kind): Href = {
+    kind match {
+      case Kind.Primitive(name) => {
         Href(name, s"/doc/types#${UrlKey.generate(name)}")
       }
-      case Type(Kind.Enum, name) => {
+      case Kind.Enum(name) => {
         Href(name, prefix(orgKey, appKey, version) + s"#enum-${UrlKey.generate(name)}")
       }
-      case Type(Kind.Model, name) => {
+      case Kind.Model(name) => {
         Href(name, prefix(orgKey, appKey, version) + s"#model-${UrlKey.generate(name)}")
       }
-      case Type(Kind.Union, name) => {
+      case Kind.Union(name) => {
         Href(name, prefix(orgKey, appKey, version) + s"#union-${UrlKey.generate(name)}")
+      }
+      case Kind.List(_) | Kind.Map(_) => {
+        sys.error(s"Did not expect list or map as final type for ${prefix(orgKey, appKey, version)} kind[$kind]")
       }
     }
   }
