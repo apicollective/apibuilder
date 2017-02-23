b9653bbaf7cb42a67d81f79faa29ba81c3c389fb
Michael Bryzek <mbryzek@alum.mit.edu>
2017-02-23 16:43:53 -0500

    Add docs on using apidoc tokens (#568)

--- www/app/controllers/DocController.scala
+++ www/app/controllers/DocController.scala
@@ -57,4 +57,8 @@ class DocController extends Controller {
     Ok(views.html.doc.playUnionTypes(request.user))
   }
 
+  def apiTokens = Anonymous { implicit request =>
+    Ok(views.html.doc.apiTokens(request.user))
+  }
+
 }
