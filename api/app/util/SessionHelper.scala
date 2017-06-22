aca49d195a494816f0b4174f970421aa6c6b6a47
Michael Bryzek <mbryzek@alum.mit.edu>
2017-06-22 18:34:25 -0400

    Implement session for forgot pwd

---
+++ api/app/util/SessionHelper.scala
@@ -0,0 +1,34 @@
+package util
+
+import javax.inject.{Inject, Singleton}
+import com.bryzek.apidoc.api.v0.models.{Authentication, User}
+import org.joda.time.DateTime
+
+@Singleton
+class SessionHelper @Inject() (
+  sessionsDao: db.generated.SessionsDao,
+  usersDao: db.UsersDao
+) {
+
+  private[this] val DefaultSessionExpirationHours = 24 * 30
+
+  def createAuthentication(u: User): Authentication = {
+    val id = SessionIdGenerator.generate()
+
+    sessionsDao.insert(
+      usersDao.AdminUser.guid,
+      _root_.db.generated.SessionForm(
+        id = id,
+        userGuid = u.guid,
+        expiresAt = DateTime.now().plusHours(DefaultSessionExpirationHours)
+      )
+    )
+
+    val dbSession = sessionsDao.findById(id).getOrElse {
+      sys.error("Failed to create session")
+    }
+
+    Conversions.toAuthentication(dbSession, u)
+  }
+  
+}
