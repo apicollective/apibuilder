b30cb81133fb22e0b633e975690bbc011a2c377d
Michael Bryzek <mbryzek@alum.mit.edu>
2017-07-28 19:36:28 -0400

    WIP

--- api/app/db/OrganizationAttributeValuesDao.scala
+++ api/app/db/OrganizationAttributeValuesDao.scala
@@ -2,6 +2,7 @@ package db
 
 import io.apibuilder.api.v0.models.{Attribute, AttributeSummary, AttributeValue, AttributeValueForm, Organization, User}
 import io.apibuilder.common.v0.models.Audit
+import io.flow.postgresql.Query
 import lib.Validation
 import anorm._
 import javax.inject.{Inject, Singleton}
@@ -15,16 +16,15 @@ class OrganizationAttributeValuesDao @Inject() (
   attributesDao: AttributesDao
 ) {
 
-  private[db] val BaseQuery = s"""
+  private[this] val BaseQuery = Query(s"""
     select organization_attribute_values.guid,
            organization_attribute_values.value,
-           ${AuditsDao.query("organization_attribute_values")},
+           ${AuditsParserDao.query("organization_attribute_values")},
            attributes.guid as attribute_guid,
            attributes.name as attribute_name
       from organization_attribute_values
       join attributes on attributes.deleted_at is null and attributes.guid = organization_attribute_values.attribute_guid
-     where true
-  """
+  """)
 
   private[this] val InsertQuery = """
     insert into organization_attribute_values
@@ -135,58 +135,20 @@ class OrganizationAttributeValuesDao @Inject() (
     limit: Long = 25,
     offset: Long = 0
   ): Seq[AttributeValue] = {
-    var nameBinds = scala.collection.mutable.ListBuffer[NamedParameter]()
-
-    val sql = Seq(
-      Some(BaseQuery.trim),
-      guid.map { v => "and organization_attribute_values.guid = {guid}::uuid" },
-      organizationGuid.map { v => "and organization_attribute_values.organization_guid = {organization_guid}::uuid" },
-      attributeGuid.map { v => "and organization_attribute_values.attribute_guid = {attribute_guid}::uuid" },
-      attributeNames.map { names =>
-        names match {
-          case Nil => {
-            "and false"
-          }
-          case _ => {
-            val clause = names.zipWithIndex.map { case (name, i) =>
-              val key = s"name_$i"
-              nameBinds += NamedParameter(key, name)
-              s"{$key}"
-            }.mkString(", ")
-            s"and organization_attribute_values.attribute_guid = (select guid from attributes where deleted_at is null and name in ($clause))"
-          }
-        }
-      },
-      isDeleted.map(Filters.isDeleted("organization_attribute_values", _)),
-      Some(s"order by lower(organization_attribute_values.value), organization_attribute_values.created_at limit ${limit} offset ${offset}")
-    ).flatten.mkString("\n   ")
-
-    val bind = Seq[Option[NamedParameter]](
-      guid.map('guid -> _.toString),
-      organizationGuid.map('organization_guid -> _.toString),
-      attributeGuid.map('attribute_guid -> _.toString)
-    ).flatten ++ nameBinds
-
     DB.withConnection { implicit c =>
-      sys.error("TODO PARSER") // SQL(sql).on(bind: _*)().toList.map { fromRow(_) }.toSeq
+      BaseQuery.
+        equals("organization_attribute_values.guid::uuid", guid).
+        equals("organization_attribute_values.organization_guid::uuid", organizationGuid).
+        equals("organization_attribute_values.attribute_guid::uuid", attributeGuid).
+        optionalIn("attributes.name", attributeNames).
+        and(isDeleted.map(Filters2.isDeleted("organization_attribute_values", _))).
+        orderBy("lower(organization_attribute_values.value), organization_attribute_values.created_at").
+        limit(limit).
+        offset(offset).
+        anormSql().as(
+          io.apibuilder.api.v0.anorm.parsers.AttributeValue.parser().*
+        )
     }
-  }
-
-  private[db] def fromRow(
-    row: anorm.Row,
-    prefix: Option[String] = None
-  ): AttributeValue = {
-    val p = prefix.map( _ + "_").getOrElse("")
-
-    AttributeValue(
-      guid = row[UUID](s"${p}guid"),
-      attribute = AttributeSummary(
-        guid = row[UUID](s"${p}attribute_guid"),
-        name = row[String](s"${p}attribute_name")
-      ),
-      value = row[String](s"${p}value"),
-      audit = AuditsDao.fromRow(row, prefix)
-    )
   }
 
 }
