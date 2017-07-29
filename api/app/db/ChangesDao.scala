3182d7a6cbf5b6a5c6af8515af078a6c78677b38
Michael Bryzek <mbryzek@alum.mit.edu>
2017-07-28 23:48:27 -0400

    fix

--- api/app/db/ChangesDao.scala
+++ api/app/db/ChangesDao.scala
@@ -1,45 +1,43 @@
 package db
 
-import io.apibuilder.api.v0.models.{Application, Change, ChangeVersion, User, UserSummary, Version}
+import io.apibuilder.api.v0.models.{Change, User, Version}
 import io.apibuilder.api.v0.models.{Diff, DiffBreaking, DiffNonBreaking, DiffUndefinedType}
-import io.apibuilder.common.v0.models.{Reference, ReferenceGuid}
-import io.apibuilder.internal.v0.models.json._
 import anorm._
 import anorm.JodaParameterMetaData._
-import javax.inject.{Inject, Named, Singleton}
+import javax.inject.{Inject, Singleton}
 
 import play.api.db._
 import play.api.Play.current
-import play.api.libs.json._
 import java.util.UUID
 
 import io.flow.postgresql.Query
 import lib.VersionTag
+import org.joda.time.DateTime
 import org.postgresql.util.PSQLException
 
 import scala.util.{Failure, Success, Try}
-import org.joda.time.DateTime
 
 @Singleton
 class ChangesDao @Inject() () {
 
-  private[this] val BaseQuery = Query(s"""
+  private[this] val BaseQuery = Query(
+    s"""
     select changes.guid,
            changes.type,
            changes.description,
            changes.changed_at,
            changes.changed_by_guid::uuid,
-           ${AuditsDao.queryCreation("changes")},
+           ${AuditsDao.queryCreationDefaultingUpdatedAt("changes")},
            applications.guid as application_guid,
            applications.key as application_key,
            organizations.guid as organization_guid,
            organizations.key as organization_key,
-           from_version.guid::text as from_guid,
-           from_version.version as from_version,
-           to_version.guid::text as to_guid,
-           to_version.version as to_version,
-           users.guid as user_guid,
-           users.nickname as user_nickname
+           from_version.guid::text as from_version_guid,
+           from_version.version as from_version_version,
+           to_version.guid::text as to_version_guid,
+           to_version.version as to_version_version,
+           users.guid as changed_by_guid,
+           users.nickname as changed_by_nickname
       from changes
       join applications on applications.guid = changes.application_guid and applications.deleted_at is null
       join organizations on organizations.guid = applications.organization_guid and organizations.deleted_at is null
@@ -48,7 +46,8 @@ class ChangesDao @Inject() () {
       join users on users.guid = changes.changed_by_guid
   """)
 
-  private[this] val InsertQuery = """
+  private[this] val InsertQuery =
+    """
     insert into changes
     (guid, application_guid, from_version_guid, to_version_guid, type, description, changed_at, changed_by_guid, created_by_guid)
     values
@@ -137,12 +136,13 @@ class ChangesDao @Inject() () {
       authorization.applicationFilter(BaseQuery).
         equals("changes.guid", guid).
         equals("organizations.guid", organizationGuid).
+        equals("organizations.key", organizationKey).
         equals("applications.guid", applicationGuid).
         equals("applications.key", applicationKey).
         equals("changes.from_version_guid", fromVersionGuid).
         equals("changes.to_version_guid", toVersionGuid).
-        greaterThanOrEquals("from_version.version_sort_key", fromVersion).
-        lessThanOrEquals("to_version.version_sort_key", toVersion).
+        greaterThanOrEquals("from_version.version_sort_key", fromVersion.map(VersionTag(_).sortKey)).
+        lessThanOrEquals("to_version.version_sort_key", toVersion.map(VersionTag(_).sortKey)).
         equals("changes.type", `type`).
         and(
           description.map { _ =>
@@ -153,9 +153,39 @@ class ChangesDao @Inject() () {
         limit(limit).
         offset(offset).
         anormSql().as(
-          io.apibuilder.api.v0.anorm.parsers.Change.parser().*
-        )
+        parser().*
+      )
     }
   }
 
+  private[this] def parser(): RowParser[io.apibuilder.api.v0.models.Change] = {
+    SqlParser.get[UUID]("guid") ~
+      io.apibuilder.common.v0.anorm.parsers.Reference.parserWithPrefix("organization") ~
+      io.apibuilder.common.v0.anorm.parsers.Reference.parserWithPrefix("application") ~
+      io.apibuilder.api.v0.anorm.parsers.ChangeVersion.parserWithPrefix("from_version") ~
+      io.apibuilder.api.v0.anorm.parsers.ChangeVersion.parserWithPrefix("to_version") ~
+      SqlParser.str("type") ~
+      SqlParser.str("description") ~
+      SqlParser.get[DateTime]("changed_at") ~
+      io.apibuilder.api.v0.anorm.parsers.UserSummary.parserWithPrefix("changed_by") ~
+      io.apibuilder.common.v0.anorm.parsers.Audit.parserWithPrefix("audit") map {
+      case guid ~ organization ~ application ~ fromVersion ~ toVersion ~ diffType ~ diffDescription ~ changedAt ~ changedBy ~ audit => {
+        io.apibuilder.api.v0.models.Change(
+          guid = guid,
+          organization = organization,
+          application = application,
+          fromVersion = fromVersion,
+          toVersion = toVersion,
+          diff = diffType match {
+            case "breaking" => DiffBreaking(description = diffDescription)
+            case "non_breaking" => DiffNonBreaking(description = diffDescription)
+            case other => sys.error(s"Unknown diff type[$other] for guid[$guid]")
+          },
+          changedAt = changedAt,
+          changedBy = changedBy,
+          audit = audit
+        )
+      }
+    }
+  }
 }
