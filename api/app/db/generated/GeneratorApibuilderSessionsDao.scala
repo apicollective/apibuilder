cf17ad4584514c08a1aab4e2cbd48b433e1a7ac7
Michael Bryzek <mbryzek@alum.mit.edu>
2017-07-30 10:05:12 -0400

    Enable compabatibility w/ flow generated DAOs

--- api/app/db/generated/GeneratorApibuilderSessionsDao.scala
+++ api/app/db/generated/GeneratorApibuilderSessionsDao.scala
@@ -1,13 +1,14 @@
 package db.generated
 
 import anorm._
-import util.Query
+import io.flow.common.v0.models.UserReference
+import io.flow.postgresql.{OrderBy, Query}
+import io.flow.postgresql.play.db.DbHelpers
 import java.sql.Connection
 import java.util.UUID
 import javax.inject.{Inject, Singleton}
 import org.joda.time.DateTime
-import play.api.db.{Database, DB, NamedDatabase}
-import play.api.Play.current
+import play.api.db.{Database, NamedDatabase}
 
 case class Session(
   id: String,
@@ -30,95 +31,101 @@ case class SessionForm(
 )
 
 @Singleton
-class SessionsDao @Inject() () {
+class SessionsDao @Inject() (
+  @NamedDatabase("default") db: Database
+) {
+
+  private[this] val dbHelpers = DbHelpers(db, "sessions")
 
   private[this] val BaseQuery = Query("""
       | select sessions.id,
       |        sessions.user_guid,
       |        sessions.expires_at,
       |        sessions.created_at,
-      |        sessions.created_by_guid,
       |        sessions.updated_at,
-      |        sessions.updated_by_guid
+      |        sessions.updated_by_user_id,
+      |        sessions.hash_code
       |   from sessions
   """.stripMargin)
 
   private[this] val InsertQuery = Query("""
     | insert into sessions
-    | (id, user_guid, expires_at, created_by_guid, updated_by_guid)
+    | (id, user_guid, expires_at, updated_by_user_id, hash_code)
     | values
-    | ({id}, {user_guid}::uuid, {expires_at}::timestamptz, {created_by_guid}::uuid, {updated_by_guid}::uuid)
+    | ({id}, {user_guid}::uuid, {expires_at}::timestamptz, {updated_by_user_id}, {hash_code}::bigint)
   """.stripMargin)
 
   private[this] val UpdateQuery = Query("""
     | update sessions
     |    set user_guid = {user_guid}::uuid,
     |        expires_at = {expires_at}::timestamptz,
-    |        updated_by_guid = {updated_by_guid}::uuid
+    |        updated_by_user_id = {updated_by_user_id},
+    |        hash_code = {hash_code}::bigint
     |  where id = {id}
+    |    and (sessions.hash_code is null or sessions.hash_code != {hash_code}::bigint)
   """.stripMargin)
 
   private[this] def bindQuery(query: Query, form: SessionForm): Query = {
     query.
       bind("user_guid", form.userGuid).
-      bind("expires_at", form.expiresAt)
+      bind("expires_at", form.expiresAt).
+      bind("hash_code", form.hashCode())
   }
 
-  def insert(updatedBy: UUID, form: SessionForm) {
-    DB.withConnection { implicit c =>
+  def insert(updatedBy: UserReference, form: SessionForm) {
+    db.withConnection { implicit c =>
       insert(c, updatedBy, form)
     }
   }
   
-  def insert(implicit c: Connection, updatedBy: UUID, form: SessionForm) {
+  def insert(implicit c: Connection, updatedBy: UserReference, form: SessionForm) {
     bindQuery(InsertQuery, form).
       bind("id", form.id).
-      bind("created_by_guid", updatedBy).
-      bind("updated_by_guid", updatedBy).
+      bind("updated_by_user_id", updatedBy.id).
       anormSql.execute()
   }
 
-  def updateIfChangedById(updatedBy: UUID, id: String, form: SessionForm) {
+  def updateIfChangedById(updatedBy: UserReference, id: String, form: SessionForm) {
     if (!findById(id).map(_.form).contains(form)) {
       updateById(updatedBy, id, form)
     }
   }
   
-  def updateById(updatedBy: UUID, id: String, form: SessionForm) {
-    DB.withConnection { implicit c =>
+  def updateById(updatedBy: UserReference, id: String, form: SessionForm) {
+    db.withConnection { implicit c =>
       updateById(c, updatedBy, id, form)
     }
   }
   
-  def updateById(implicit c: Connection, updatedBy: UUID, id: String, form: SessionForm) {
+  def updateById(implicit c: Connection, updatedBy: UserReference, id: String, form: SessionForm) {
     bindQuery(UpdateQuery, form).
       bind("id", id).
-      bind("updated_by_guid", updatedBy).
+      bind("updated_by_user_id", updatedBy.id).
       anormSql.execute()
   }
 
-  def update(updatedBy: UUID, existing: Session, form: SessionForm) {
-    DB.withConnection { implicit c =>
+  def update(updatedBy: UserReference, existing: Session, form: SessionForm) {
+    db.withConnection { implicit c =>
       update(c, updatedBy, existing, form)
     }
   }
   
-  def update(implicit c: Connection, updatedBy: UUID, existing: Session, form: SessionForm) {
+  def update(implicit c: Connection, updatedBy: UserReference, existing: Session, form: SessionForm) {
     updateById(c, updatedBy, existing.id, form)
   }
 
-  def delete(deletedBy: UUID, session: Session) {
-    delete(deletedBy, session.id)
+  def delete(deletedBy: UserReference, session: Session) {
+    dbHelpers.delete(deletedBy, session.id)
   }
   
-  def deleteById(deletedBy: UUID, id: String) {
-    DB.withConnection { implicit c =>
+  def deleteById(deletedBy: UserReference, id: String) {
+    db.withConnection { implicit c =>
       deleteById(c, deletedBy, id)
     }
   }
   
-  def deleteById(c: java.sql.Connection, deletedBy: UUID, id: String) {
-    delete(c, deletedBy, id)
+  def deleteById(c: java.sql.Connection, deletedBy: UserReference, id: String) {
+    dbHelpers.delete(c, deletedBy, id)
   }
 
   def findById(id: String): Option[Session] = {
@@ -130,41 +137,21 @@ class SessionsDao @Inject() () {
     userGuid: Option[UUID] = None,
     limit: Long,
     offset: Long = 0,
-    orderBy: String = "sessions.id"
+    orderBy: OrderBy = OrderBy("sessions.id")
   ) (
     implicit customQueryModifier: Query => Query = { q => q }
   ): Seq[Session] = {
-    DB.withConnection { implicit c =>
+    db.withConnection { implicit c =>
       customQueryModifier(BaseQuery).
         optionalIn("sessions.id", ids).
         equals("sessions.user_guid", userGuid).
         limit(limit).
         offset(offset).
-        orderBy(orderBy).
+        orderBy(orderBy.sql).
         as(SessionsDao.parser().*)
     }
   }
 
-  private[this] val DeleteQuery = s"""
-    delete from sessions where id = {id}
-  """
-
-  def delete(deletedBy: UUID, id: String) {
-    DB.withConnection { implicit c =>
-      delete(c, deletedBy, id)
-    }
-  }
-
-  def delete(
-    implicit c: java.sql.Connection,
-    deletedBy: UUID,
-    id: String
-  ) {
-    SQL(DeleteQuery).on(
-      'id -> id
-    ).execute()
-  }
-  
 }
 
 object SessionsDao {
