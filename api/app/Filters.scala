4c6647b1f00e2db8ccb4739d1b106c42956d1e01
Michael Bryzek <mbryzek@alum.mit.edu>
2018-02-09 14:47:30 +0000

    Modernize startup, filters (#727)

--- api/app/Filters.scala
+++
@@ -1,8 +0,0 @@
-import javax.inject.Inject
-
-import play.api.http.HttpFilters
-import play.filters.cors.CORSFilter
-
-class Filters @Inject() (corsFilter: CORSFilter) extends HttpFilters {
-  def filters = Seq(corsFilter)
-}
---
+++ api/app/play/LoggingFilter.scala
@@ -0,0 +1,46 @@
+package io.apicollective.play
+
+import play.api.Logger
+import play.api.mvc._
+
+import scala.concurrent.{ExecutionContext, Future}
+import play.api.http.HttpFilters
+
+/**
+  * To use in any Flow app depending on lib-play:
+  *
+  * (1) Add this to your base.conf:
+  *    play.http.filters=io.apicollective.LoggingFilter
+  *
+  **/
+
+class LoggingFilter @javax.inject.Inject() (loggingFilter: ApibuilderLoggingFilter) extends HttpFilters {
+  def filters = Seq(loggingFilter)
+}
+
+class ApibuilderLoggingFilter @javax.inject.Inject() (
+  implicit ec: ExecutionContext
+) extends Filter {
+  def apply(f: RequestHeader => Future[Result])(requestHeader: RequestHeader): Future[Result] = {
+    val startTime = System.currentTimeMillis
+    f(requestHeader).map { result =>
+      val endTime = System.currentTimeMillis
+      val requestTime = endTime - startTime
+      val headerMap = requestHeader.headers.toMap
+      val line = Seq(
+        requestHeader.method,
+        s"${requestHeader.host}${requestHeader.uri}",
+        result.header.status,
+        s"${requestTime}ms",
+        headerMap.getOrElse("X-Flow-Request-Id", Nil).mkString(","),
+        headerMap.getOrElse("User-Agent", Nil).mkString(","),
+        headerMap.getOrElse("X-Forwarded-For", Nil).mkString(","),
+        headerMap.getOrElse("CF-Connecting-IP", Nil).mkString(",")
+      ).mkString(" ")
+
+      Logger.info(line)
+
+      result
+    }
+  }
+}
