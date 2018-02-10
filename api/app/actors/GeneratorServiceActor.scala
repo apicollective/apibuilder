c9089ec2c601db547083e926840752fd8187d3d6
Michael Bryzek <mike@flow.io>
2018-02-10 14:28:59 -0500

    Remove play current

--- api/app/actors/GeneratorServiceActor.scala
+++ api/app/actors/GeneratorServiceActor.scala
@@ -2,69 +2,22 @@ package actors
 
 import akka.actor.{ActorLogging, ActorSystem}
 import lib.Pager
-import io.apibuilder.api.v0.models.{GeneratorForm, GeneratorService}
-import io.apibuilder.generator.v0.Client
-import db.{Authorization, UsersDao}
-import db.generators.{GeneratorsDao, ServicesDao}
+import db.Authorization
+import db.generators.ServicesDao
 import play.api.Logger
 import akka.actor.Actor
 import java.util.UUID
-import scala.concurrent.Await
+
+import util.GeneratorServiceUtil
+
 import scala.concurrent.duration._
+import scala.util.{Failure, Success, Try}
 
 object GeneratorServiceActor {
 
   object Messages {
     case class GeneratorServiceCreated(guid: UUID)
-    case object Sync
-  }
-
-  // TODO: Refactor and inject directly
-  private[this] def servicesDao = play.api.Play.current.injector.instanceOf[ServicesDao]
-  private[this] def generatorsDao = play.api.Play.current.injector.instanceOf[GeneratorsDao]
-  private[this] def usersDao = play.api.Play.current.injector.instanceOf[UsersDao]
-
-  def sync(serviceGuid: UUID)(implicit ec: scala.concurrent.ExecutionContext) {
-    servicesDao.findByGuid(Authorization.All, serviceGuid).map { sync }
-  }
-
-  def sync(
-    service: GeneratorService
-  ) (
-    implicit ec: scala.concurrent.ExecutionContext
-  ) {
-    val client = new Client(service.uri)
-
-    var iteration = 0
-    val limit = 100
-    var num = limit
-    var offset = 0
-
-    while (num >= limit) {
-      val generators = Await.result(
-        client.generators.get(limit = limit, offset = offset),
-        30.seconds
-      )
-      num = generators.size
-      offset += limit
-      iteration += 1
-      if (iteration > 25) {
-        sys.error(s"Likely infinite loop fetching generators for service at URI[${service.uri}]")
-      }
-
-      generators.foreach { gen =>
-        generatorsDao.upsert(
-          usersDao.AdminUser,
-          GeneratorForm(
-            serviceGuid = service.guid,
-            generator = gen
-          )
-        ) match {
-          case Left(errors) => Logger.error(s"Error fetching generators for service[${service.guid}] uri[${service.uri}]: " + errors.mkString(", "))
-          case Right(_) => {}
-        }
-      }
-    }
+    case object SyncAll
   }
 
 }
@@ -72,28 +25,38 @@ object GeneratorServiceActor {
 @javax.inject.Singleton
 class GeneratorServiceActor @javax.inject.Inject() (
   system: ActorSystem,
-  usersDao: UsersDao,
-  servicesDao: ServicesDao
+  servicesDao: ServicesDao,
+  util: GeneratorServiceUtil
 ) extends Actor with ActorLogging with ErrorHandler {
 
-  implicit val ec = system.dispatchers.lookup("generator-service-actor-context")
+  private[this] implicit val ec = system.dispatchers.lookup("generator-service-actor-context")
 
-  system.scheduler.schedule(1.hour, 1.hour, self, GeneratorServiceActor.Messages.Sync)
+  system.scheduler.schedule(1.hour, 1.hour, self, GeneratorServiceActor.Messages.SyncAll)
 
   def receive = {
 
     case m @ GeneratorServiceActor.Messages.GeneratorServiceCreated(guid) => withVerboseErrorHandler(m) {
-      GeneratorServiceActor.sync(guid)
+      util.sync(guid)
     }
 
-    case m @ GeneratorServiceActor.Messages.Sync => withVerboseErrorHandler(m) {
+    case m @ GeneratorServiceActor.Messages.SyncAll => withVerboseErrorHandler(m) {
       Pager.eachPage { offset =>
         servicesDao.findAll(
           Authorization.All,
+          limit = 200,
           offset = offset
         )
       } { service =>
-        GeneratorServiceActor.sync(service.guid)
+        Try {
+          util.sync(service)
+        } match {
+          case Success(_) => {
+            Logger.info(s"[GeneratorServiceActor] Service[${service.guid}] at uri[${service.uri}] synced")
+          }
+          case Failure(ex) => {
+            Logger.error(s"[GeneratorServiceActor] Service[${service.guid}] at uri[${service.uri}] failed to sync: ${ex.getMessage}", ex)
+          }
+        }
       }
     }
 
---
+++ api/app/util/GeneratorServiceUtil.scala
@@ -0,0 +1,63 @@
+package util
+
+import java.util.UUID
+import javax.inject.Inject
+
+import db.{Authorization, UsersDao}
+import db.generators.{GeneratorsDao, ServicesDao}
+import io.apibuilder.api.v0.models.{GeneratorForm, GeneratorService}
+import io.apibuilder.generator.v0.Client
+import play.api.Logger
+
+import scala.concurrent.Await
+import scala.concurrent.duration.{FiniteDuration, SECONDS}
+
+class GeneratorServiceUtil @Inject() (
+  servicesDao: ServicesDao,
+  generatorsDao: GeneratorsDao,
+  usersDao: UsersDao
+) {
+
+  def sync(serviceGuid: UUID)(implicit ec: scala.concurrent.ExecutionContext) {
+    servicesDao.findByGuid(Authorization.All, serviceGuid).foreach { sync }
+  }
+
+  def sync(
+    service: GeneratorService
+  ) (
+    implicit ec: scala.concurrent.ExecutionContext
+  ) {
+    val client = new Client(service.uri)
+
+    var iteration = 0
+    val limit = 100
+    var num = limit
+    var offset = 0
+
+    while (num >= limit) {
+      val generators = Await.result(
+        client.generators.get(limit = limit, offset = offset),
+        FiniteDuration(30, SECONDS)
+      )
+      num = generators.size
+      offset += limit
+      iteration += 1
+      if (iteration > 25) {
+        sys.error(s"Likely infinite loop fetching generators for service at URI[${service.uri}]")
+      }
+
+      generators.foreach { gen =>
+        generatorsDao.upsert(
+          usersDao.AdminUser,
+          GeneratorForm(
+            serviceGuid = service.guid,
+            generator = gen
+          )
+        ) match {
+          case Left(errors) => Logger.error(s"Error fetching generators for service[${service.guid}] uri[${service.uri}]: " + errors.mkString(", "))
+          case Right(_) => {}
+        }
+      }
+    }
+  }
+}
