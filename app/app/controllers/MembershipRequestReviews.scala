7e0597d6680e0fbb8c369f3753a23fc6d8c6c22d
Michael Bryzek <mike@flow.io>
2018-02-12 08:28:43 -0500

    wip

--- app/app/controllers/MembershipRequestReviews.scala
+++ app/app/controllers/MembershipRequestReviews.scala
@@ -10,22 +10,22 @@ class MembershipRequestReviews @Inject() (
   private[this] implicit val ec = scala.concurrent.ExecutionContext.Implicits.global
 
   def accept(orgKey: String, membershipRequestGuid: UUID) = IdentifiedOrg.async { implicit request =>
-    require(request.isAdmin, "You are not an administrator")
-
-    for {
-      _ <- request.api.MembershipRequests.postAcceptByGuid(membershipRequestGuid)
-    } yield {
-      Redirect(routes.Organizations.membershipRequests(orgKey)).flashing("success" -> "Request accepted")
+    request.withAdmin {
+      for {
+        _ <- request.api.MembershipRequests.postAcceptByGuid(membershipRequestGuid)
+      } yield {
+        Redirect(routes.Organizations.membershipRequests(orgKey)).flashing("success" -> "Request accepted")
+      }
     }
   }
 
   def decline(orgKey: String, membershipRequestGuid: UUID) = IdentifiedOrg.async { implicit request =>
-    require(request.isAdmin, "You are not an administrator")
-
-    for {
-      review <- request.api.MembershipRequests.postDeclineByGuid(membershipRequestGuid)
-    } yield {
-      Redirect(routes.Organizations.membershipRequests(orgKey)).flashing("success" -> "Request declined")
+    request.withAdmin {
+      for {
+        _ <- request.api.MembershipRequests.postDeclineByGuid(membershipRequestGuid)
+      } yield {
+        Redirect(routes.Organizations.membershipRequests(orgKey)).flashing("success" -> "Request declined")
+      }
     }
   }
 
--- app/app/controllers/Organizations.scala
+++ app/app/controllers/Organizations.scala
@@ -12,6 +12,7 @@ import scala.concurrent.Future
 import javax.inject.Inject
 
 import io.apibuilder.api.v0.Client
+
 class Organizations @Inject() (
   val apibuilderControllerComponents: ApibuilderControllerComponents,
   apiClientProvider: ApiClientProvider,
@@ -28,7 +29,7 @@ class Organizations @Inject() (
       limit = Pagination.DefaultLimit+1,
       offset = page * Pagination.DefaultLimit
     ).flatMap { applications =>
-      hasMembershipRequests(request.api, request.isAdmin, request.org.guid).map { haveMembershipRequests =>
+      hasMembershipRequests(request.api, request.requestData.isAdmin, request.org.guid).map { haveMembershipRequests =>
         Ok(
           views.html.organizations.show(
             request.mainTemplate().copy(title = Some(request.org.name)),
@@ -41,26 +42,26 @@ class Organizations @Inject() (
   }
 
   def details(orgKey: String) = IdentifiedOrg.async { implicit request =>
-    hasMembershipRequests(request.api, request.isAdmin, request.org.guid).map { haveMembershipRequests =>
+    hasMembershipRequests(request.api, request.requestData.isAdmin, request.org.guid).map { haveMembershipRequests =>
       val tpl = request.mainTemplate().copy(settings = Some(SettingsMenu(section = Some(SettingSection.Details))))
       Ok(views.html.organizations.details(tpl, request.org, haveMembershipRequests = haveMembershipRequests))
     }
   }
 
   def membershipRequests(orgKey: String, page: Int = 0) = IdentifiedOrg.async { implicit request =>
-    request.requireAdmin
-
-    for {
-      requests <- request.api.MembershipRequests.get(
-        orgKey = Some(orgKey),
-        limit = Pagination.DefaultLimit+1,
-        offset = page * Pagination.DefaultLimit
-      )
-    } yield {
-      Ok(views.html.organizations.membershipRequests(
-        request.mainTemplate(Some(request.org.name)),
-        requests = PaginatedCollection(page, requests))
-      )
+    request.withAdmin {
+      for {
+        requests <- request.api.MembershipRequests.get(
+          orgKey = Some(orgKey),
+          limit = Pagination.DefaultLimit + 1,
+          offset = page * Pagination.DefaultLimit
+        )
+      } yield {
+        Ok(views.html.organizations.membershipRequests(
+          request.mainTemplate(Some(request.org.name)),
+          requests = PaginatedCollection(page, requests))
+        )
+      }
     }
   }
 
@@ -87,8 +88,8 @@ class Organizations @Inject() (
           Redirect("/").flashing("warning" -> s"Organization $orgKey not found")
         }
         case Some(org: Organization) => {
-          val isMember = !membershipsResponse.headOption.isEmpty
-          val hasMembershipRequest = !membershipRequestResponse.isEmpty
+          val isMember = membershipsResponse.nonEmpty
+          val hasMembershipRequest = membershipRequestResponse.nonEmpty
           Ok(views.html.organizations.requestMembership(
             request.mainTemplate(Some(s"Join ${org.name}")),
             org,
@@ -163,63 +164,59 @@ class Organizations @Inject() (
   }
 
   def edit(orgKey: String) = Identified.async { implicit request =>
-    apiClientProvider.callWith404(request.api.Organizations.getByKey(orgKey)).map { orgOption =>
-      orgOption match {
-        case None => {
-          Redirect(routes.ApplicationController.index()).flashing("warning" -> "Org not found")
-        }
-        case Some(org) => {
-          val filledForm = Organizations.orgForm.fill(
-            Organizations.OrgData(
-              name = org.name,
-              namespace = org.namespace,
-              key = Some(org.key),
-              visibility = org.visibility.toString
-            )
+    apiClientProvider.callWith404(request.api.Organizations.getByKey(orgKey)).map {
+      case None => {
+        Redirect(routes.ApplicationController.index()).flashing("warning" -> "Org not found")
+      }
+      case Some(org) => {
+        val filledForm = Organizations.orgForm.fill(
+          Organizations.OrgData(
+            name = org.name,
+            namespace = org.namespace,
+            key = Some(org.key),
+            visibility = org.visibility.toString
           )
-          Ok(views.html.organizations.edit(request.mainTemplate().copy(org = Some(org)), org, filledForm))
-        }
+        )
+        Ok(views.html.organizations.edit(request.mainTemplate().copy(org = Some(org)), org, filledForm))
       }
     }
   }
 
   def editPost(orgKey: String) = Identified.async { implicit request =>
-    apiClientProvider.callWith404(request.api.Organizations.getByKey(orgKey)).flatMap { orgOption =>
-      orgOption match {
-        case None => Future {
-          Redirect(routes.ApplicationController.index()).flashing("warning" -> "Org not found")
-        }
-        case Some(org) => {
-          val tpl = request.mainTemplate(Some("Edit Organization"))
+    apiClientProvider.callWith404(request.api.Organizations.getByKey(orgKey)).flatMap {
+      case None => Future.successful {
+        Redirect(routes.ApplicationController.index()).flashing("warning" -> "Org not found")
+      }
+      case Some(org) => {
+        val tpl = request.mainTemplate(Some("Edit Organization"))
 
-          val form = Organizations.orgForm.bindFromRequest
-          form.fold (
+        val form = Organizations.orgForm.bindFromRequest
+        form.fold(
 
-            errors => Future {
-              Ok(views.html.organizations.edit(tpl, org, errors))
-            },
+          errors => Future {
+            Ok(views.html.organizations.edit(tpl, org, errors))
+          },
 
-            valid => {
-              val updatedKey = valid.key.map(_.trim).getOrElse(org.key)
+          valid => {
+            val updatedKey = valid.key.map(_.trim).getOrElse(org.key)
 
-              request.api.Organizations.putByKey(
-                key = org.key,
-                organizationForm = OrganizationForm(
-                  name = valid.name,
-                  namespace = valid.namespace,
-                  key = Some(updatedKey),
-                  visibility = Visibility(valid.visibility)
-                )
-              ).map { org =>
-                Redirect(routes.Organizations.details(updatedKey)).flashing("success" -> "Org updated")
-              }.recover {
-                case r: io.apibuilder.api.v0.errors.ErrorsResponse => {
-                  Ok(views.html.organizations.edit(tpl, org, form, r.errors.map(_.message)))
-                }
+            request.api.Organizations.putByKey(
+              key = org.key,
+              organizationForm = OrganizationForm(
+                name = valid.name,
+                namespace = valid.namespace,
+                key = Some(updatedKey),
+                visibility = Visibility(valid.visibility)
+              )
+            ).map { _ =>
+              Redirect(routes.Organizations.details(updatedKey)).flashing("success" -> "Org updated")
+            }.recover {
+              case r: io.apibuilder.api.v0.errors.ErrorsResponse => {
+                Ok(views.html.organizations.edit(tpl, org, form, r.errors.map(_.message)))
               }
             }
-          )
-        }
+          }
+        )
       }
     }
   }
@@ -234,7 +231,7 @@ class Organizations @Inject() (
 
   private[this] def hasMembershipRequests(api: Client, isAdmin: Boolean, orgGuid: UUID): Future[Boolean] = {
     if (isAdmin) {
-      api.MembershipRequests.get(orgGuid = Some(orgGuid), limit = 1).map(!_.isEmpty)
+      api.MembershipRequests.get(orgGuid = Some(orgGuid), limit = 1).map(_.nonEmpty)
     } else {
       Future.successful(false)
     }
