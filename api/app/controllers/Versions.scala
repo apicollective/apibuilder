c74b9b7143b3c18b71bbf723fc8f0bfb5de6a634
Gregor Heine <gheine@users.noreply.github.com>
2018-07-19 11:16:43 +0100

    Prefix types of fields in imported models with full namespace (#761)

--- api/app/controllers/Versions.scala
+++ api/app/controllers/Versions.scala
@@ -2,7 +2,7 @@ package controllers
 
 import io.apibuilder.api.v0.models.{ApplicationForm, Error, Organization, Original, User, Version, VersionForm, Visibility}
 import io.apibuilder.api.v0.models.json._
-import io.apibuilder.spec.v0.models.{Service, UnionType}
+import io.apibuilder.spec.v0.models.{Field, Service, UnionType}
 import lib._
 import builder.OriginalValidator
 import db._
@@ -62,20 +62,30 @@ class Versions @Inject() (
         }
 
         val service = resolveChildren(v.service).foldLeft(v.service) { case (service, (namespace, child)) =>
-          def prefixUnionType(unionType: UnionType): UnionType = {
-            if (child.models.exists(_.name == unionType.`type`)) {
-              unionType.copy(`type` = s"$namespace.models.${unionType.`type`}")
-            } else if (child.enums.exists(_.name == unionType.`type`)) {
-              unionType.copy(`type` = s"$namespace.enums.${unionType.`type`}")
-            } else if (child.unions.exists(_.name == unionType.`type`)) {
-              unionType.copy(`type` = s"$namespace.unions.${unionType.`type`}")
-            } else {
-              unionType
+
+          def fixType[T](origTyp: String, update: String => T): T = {
+            val ArrayRx = """(\[?)(.*?)(\]?)""".r
+            origTyp match {
+              case ArrayRx(pre, typ, pst) =>
+                if (child.models.exists(_.name == typ)) {
+                  update(s"$pre$namespace.models.$typ$pst")
+                } else if (child.enums.exists(_.name == typ)) {
+                  update(s"$pre$namespace.enums.$typ$pst")
+                } else if (child.unions.exists(_.name == typ)) {
+                  update(s"$pre$namespace.unions.$typ$pst")
+                } else {
+                  update(origTyp)
+                }
+              case _ => update(origTyp)
             }
           }
+
+          def prefixUnionType(unionType: UnionType) = fixType(unionType.`type`, typ => unionType.copy(`type` = typ))
+          def prefixModelFields(field: Field)       = fixType(field.`type`, typ => field.copy(`type` = typ))
+
           service.copy(
             enums = service.enums ++ child.enums.map(e => e.copy(name = s"$namespace.enums.${e.name}")),
-            models = service.models ++ child.models.map(m => m.copy(name = s"$namespace.models.${m.name}")),
+            models = service.models ++ child.models.map(m => m.copy(name = s"$namespace.models.${m.name}", fields = m.fields.map(prefixModelFields))),
             unions = service.unions ++ child.unions.map(u => u.copy(name = s"$namespace.unions.${u.name}", types = u.types.map(prefixUnionType)))
           )
         }
@@ -85,6 +95,7 @@ class Versions @Inject() (
         } else {
           ExampleJson.requiredFieldsOnly(service)
         }
+
         example.sample(typeName, subTypeName) match {
           case None => NotFound
           case Some(js) => Ok(js)
