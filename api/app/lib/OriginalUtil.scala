7b6a32c933da5ddea48bf7cd224af6c602075dbd
Michael Bryzek <mike@flow.io>
2020-05-09 14:05:15 -0400

    tests

--- api/app/lib/OriginalUtil.scala
+++ api/app/lib/OriginalUtil.scala
@@ -1,10 +1,13 @@
 package lib
 
+import io.apibuilder.api.json.v0.models.ApiJson
+import io.apibuilder.api.json.v0.models.json._
 import io.apibuilder.api.v0.models.{Original, OriginalForm, OriginalType}
 import io.apibuilder.spec.v0.models.Service
 import io.apibuilder.spec.v0.models.json._
-import play.api.libs.json.{Json, JsString, JsObject, JsSuccess}
-import scala.util.{Failure, Success, Try}
+import play.api.libs.json.{JsArray, JsObject, JsString, JsSuccess, Json}
+
+import scala.util.{Success, Try}
 
 object OriginalUtil {
 
@@ -26,29 +29,47 @@ object OriginalUtil {
       Some(OriginalType.AvroIdl)
     } else if (trimmed.startsWith("{")) {
       Try(
-        Json.parse(trimmed).asOpt[JsObject] match {
-          case None => None
-          case Some(o) => {
-            (o \ "swagger").asOpt[JsString] match {
-              case Some(_) => Some(OriginalType.Swagger)
-              case None => {
-                o.validate[Service] match {
-                  case JsSuccess(_, _) => Some(OriginalType.ServiceJson)
-                  case _ => Some(OriginalType.ApiJson)
-                }
-              }
-            }
+        Json.parse(trimmed)
+      ) match {
+        case Success(o: JsObject) => {
+          (o \ "swagger").asOpt[JsString] match {
+            case Some(_) => Some(OriginalType.Swagger)
+            case None => guessApiOrServiceJson(o)
           }
         }
-      ) match {
-        case Success(ot) => ot
-        case Failure(_) => None
+        case _ => None
       }
     } else if (trimmed.contains("swagger:")) {
       Some(OriginalType.Swagger)
     } else {
       None
     }
+  }
+
+  def guessApiOrServiceJson(o: JsObject): Option[OriginalType] = {
+    o.validate[Service] match {
+      case JsSuccess(_, _) => Some(OriginalType.ServiceJson)
+      case _ => {
+        o.validate[ApiJson] match {
+          case JsSuccess(_, _) => Some(OriginalType.ApiJson)
+          case _ => {
+            // service.json has these defined as array; api.json as maps
+            val fields = Seq("enums", "interfaces", "unions", "models")
+
+            val arrays = fields.flatMap { f => (o \ f).asOpt[JsArray] }
+            val objects = fields.flatMap { f => (o \ f).asOpt[JsObject] }
+            if (arrays.nonEmpty && objects.isEmpty) {
+              Some(OriginalType.ServiceJson)
+            } else if (arrays.isEmpty && objects.nonEmpty) {
+              Some(OriginalType.ApiJson)
+            } else {
+              None
+            }
+          }
+        }
+      }
+    }
+
   }
 
 }
