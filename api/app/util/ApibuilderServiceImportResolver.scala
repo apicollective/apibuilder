9971e255562efe167b64199418accf8e603c55f6
Michael Bryzek <mike@flow.io>
2020-06-04 17:51:15 -0400

    fix

--- api/app/util/ApibuilderServiceImportResolver.scala
+++ api/app/util/ApibuilderServiceImportResolver.scala
@@ -2,34 +2,84 @@ package util
 
 import db.{Authorization, VersionsDao}
 import io.apibuilder.api.v0.models.Version
-import io.apibuilder.spec.v0.models.Service
+import io.apibuilder.spec.v0.models.{Import, Service}
+import io.flow.log.RollbarLogger
 import javax.inject.Inject
+import lib.VersionTag
+
+import scala.annotation.tailrec
 
 class ApibuilderServiceImportResolver @Inject() (
   versionsDao: VersionsDao,
+  rollbarLogger: RollbarLogger,
 ) {
+  private[this] val logger = rollbarLogger.fingerprint(getClass.getName)
 
   /**
-   * Expands each import into its service definition
+   * Expands each import into its service definition, returning the list of imported services.
+   * If a service is imported more than once, returns the latest version.
    **/
   def resolve(auth: Authorization, service: Service): Seq[Service] = {
-    resolve(auth, service, Map.empty).values.toSeq
+    resolve(auth, service.imports.toList, ServiceBuilder()).distinctServicesByLatest
   }
 
-  private[this] def resolve(auth: Authorization, service: Service, acc: Map[String, Service]): Map[String, Service] = {
-    println(s"Service ${service.name}: ${service.imports.map(_.namespace)}")
-    service.imports.foldLeft(acc) { case (acc, imp) =>
-      if (acc.contains(imp.namespace)) {
-        acc
-      } else {
-        versionsDao.findVersion(auth, imp.organization.key, imp.application.key, imp.version) match {
-          case None => {
-            println(s"Could not resolve import: ${imp.organization.key}/${imp.application.key}/${imp.version}")
-            acc
+  @tailrec
+  private[this] def resolve(auth: Authorization, imports: List[Import], builder: ServiceBuilder): ServiceBuilder = {
+    imports match {
+      case Nil => builder
+      case imp :: rest => {
+        if (builder.hasImport(imp)) {
+          resolve(auth, rest, builder)
+        } else {
+          versionsDao.findVersion(auth, imp.organization.key, imp.application.key, imp.version) match {
+            case None => {
+              logger
+                .organization(imp.organization.key)
+                .withKeyValue("application", imp.application.key)
+                .withKeyValue("version", imp.version)
+                .info("Could not resolve import")
+              resolve(auth, rest, builder)
+            }
+            case Some(v: Version) => {
+              resolve(auth, rest, builder.withService(v.service))
+            }
           }
-          case Some(v: Version) => resolve(auth, v.service, acc + (imp.namespace -> v.service))
         }
       }
     }
   }
+}
+
+private[util] case class ServiceBuilder(services: Seq[Service] = Nil) {
+
+  private[this] val keysWithVersion: Set[String] = services.map(toKeyWithVersion).toSet
+
+  private[this] def toKeyWithVersion(service: Service): String = {
+    s"${service.organization.key}/${service.application.key}/${service.version}"
+  }
+  private[this] def toKeyWithVersion(imp: Import): String = {
+    s"${imp.organization.key}/${imp.application.key}/${imp.version}"
+  }
+
+  def hasImport(imp: Import): Boolean = {
+    keysWithVersion.contains(toKeyWithVersion(imp))
+  }
+
+  /**
+   * Returns the list of distinct services. Where multiple versions are available,
+   * chooses the latest.
+   */
+  lazy val distinctServicesByLatest: Seq[Service] = {
+    services
+      .groupBy { s => s"${s.organization.key}/${s.application.key}" }
+      .map { case (_, services) =>
+        services.maxBy { s => VersionTag(s.version) }
+      }.toSeq
+  }
+
+
+  def withService(service: Service): ServiceBuilder = {
+    ServiceBuilder(services = services ++ Seq(service))
+  }
+
 }
--- api/test/util/ApibuilderServiceImportResolverSpec.scala
+++ api/test/util/ApibuilderServiceImportResolverSpec.scala
@@ -13,12 +13,13 @@ class ApibuilderServiceImportResolverSpec extends PlaySpec with GuiceOneAppPerSu
 
   private[this] def apibuilderServiceImportResolver: ApibuilderServiceImportResolver = app.injector.instanceOf[ApibuilderServiceImportResolver]
 
-  private[this] def createUserServiceVersion(version: String): Service = {
+  private[this] def createServiceVersion(name: String = "user", version: String): Service = {
     val svc = makeService(
       organization = makeOrganization(key = "test"),
+      namespace = "test.com",
       application = makeApplication(key = "user"),
       version = version,
-      name = "user",
+      name = name,
     )
     createVersion(svc)
     svc
@@ -41,7 +42,7 @@ class ApibuilderServiceImportResolverSpec extends PlaySpec with GuiceOneAppPerSu
       makeService(
         imports = Seq(
           makeImport(
-            createUserServiceVersion(version = "1.0.0")
+            createServiceVersion(version = "1.0.0")
           ),
         )
       )
@@ -51,7 +52,7 @@ class ApibuilderServiceImportResolverSpec extends PlaySpec with GuiceOneAppPerSu
   }
 
   "collapses duplicate imports" in {
-    val userService = createUserServiceVersion(version = "1.0.0")
+    val userService = createServiceVersion(version = "1.0.0")
     val service = makeService(
       imports = Seq(
         makeImport(userService),
@@ -61,20 +62,41 @@ class ApibuilderServiceImportResolverSpec extends PlaySpec with GuiceOneAppPerSu
     resolve(service) must equal(Seq("test/user/1.0.0"))
   }
 
+  "keeps different services" in {
+    val userService = createServiceVersion(version = "1.0.0", name = "user")
+    val orgService = createServiceVersion(version = "2.0.0", name = "org")
+    val service = makeService(
+      imports = Seq(
+        makeImport(userService),
+        makeImport(orgService),
+      )
+    )
+    resolve(service).sorted must equal(
+      Seq("test/org/2.0.0", "test/user/1.0.0")
+    )
+  }
+
   "selects latest version regardless of order of imports" in {
-    def test(svc1: Service, svc2: Service) = {
+    def test(svc1: Service, svc2: Service, svc3: Service) = {
       resolve(
         makeService(imports = Seq(
-          makeImport(svc1), makeImport(svc2)
+          makeImport(svc1), makeImport(svc2), makeImport(svc3)
         ))
       )
     }
 
-    val userService1 = createUserServiceVersion(version = "1.0.0")
-    val userService2 = createUserServiceVersion(version = "1.0.1")
+    val s1 = createServiceVersion(version = "1.0.0")
+    val s2 = createServiceVersion(version = "1.0.1")
+    val s3 = createServiceVersion(version = "1.0.1-dev")
 
-    test(userService1, userService2) must equal(Seq("test/user/1.0.1"))
-    test(userService2, userService1) must equal(Seq("test/user/1.0.1"))
+    test(s1, s2, s3) must equal(Seq("test/user/1.0.1"))
+    test(s1, s3, s2) must equal(Seq("test/user/1.0.1"))
+
+    test(s2, s1, s3) must equal(Seq("test/user/1.0.1"))
+    test(s2, s3, s1) must equal(Seq("test/user/1.0.1"))
+
+    test(s3, s1, s2) must equal(Seq("test/user/1.0.1"))
+    test(s3, s2, s1) must equal(Seq("test/user/1.0.1"))
   }
 
 }
