929996481211d5a4aa074960ea3f3001cf141381
Michael Bryzek <mike@flow.io>
2021-04-20 11:50:33 -0400

    depre

--- app/test/lib/TestApplication.scala
+++ app/test/lib/TestApplication.scala
@@ -1,11 +1,12 @@
 package lib
 
+import play.api.ApplicationLoader.Context
 import play.api.{ApplicationLoader, Environment, Mode, Play}
 
 trait TestApplication {
 
   private[this] val env = Environment(new java.io.File("."), this.getClass.getClassLoader, Mode.Test)
-  private[this] val context = ApplicationLoader.createContext(env)
+  private[this] val context = Context.create(env)
   private[this] val app = ApplicationLoader(context).load(context)
   Play.start(app)
 
