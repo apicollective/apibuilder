9cfc267d3005f61d15d612754b565565afcb6956
Michael Bryzek <mike@flow.io>
2021-12-12 14:21:35 -0500

    wip

--- swagger/src/test/scala/io/apibuilder/swagger/v2/OpenApi3PetStoreExampleSpec.scala
+++ swagger/src/test/scala/io/apibuilder/swagger/v2/OpenApi3PetStoreExampleSpec.scala
@@ -1,6 +1,7 @@
 package io.apibuilder.swagger.v2
 
 import _root_.helpers.{ServiceConfigurationHelpers, TestHelpers}
+import io.apibuilder.spec.v0.models.{Field, Model}
 import io.apibuilder.swagger.helpers.FileHelpers
 import lib.ServiceConfiguration
 import org.scalatest.{FunSpec, Matchers}
@@ -62,5 +63,54 @@ class OpenApi3PetStoreExampleSpec extends FunSpec with Matchers
     svc.description.get shouldBe "A sample API that uses a petstore as an example to demonstrate features in the OpenAPI 3.0 specification"
   }
 
+  it("models") {
+    def model[T](name: String)(f: Model => T): T = {
+      f(svc.models.find(_.name == name).get)
+    }
+    def field[T](model: Model, name: String)(f: Field => T): T = {
+      f(model.fields.find(_.name == name).get)
+    }
 
+    svc.models.map(_.name).sorted shouldBe Seq("Error", "NewPet", "Pet").sorted
+
+    model("Error") { m =>
+      m.plural shouldBe "Errors"
+      m.description shouldBe None
+      m.deprecation shouldBe None
+      m.fields.map(_.name) shouldBe Seq("code", "message")
+
+      field(m, "code") { f =>
+        f.required shouldBe true
+        f.`type` shouldBe "integer"
+        f.description shouldBe None
+      }
+
+      field(m, "message") { f =>
+        f.required shouldBe true
+        f.`type` shouldBe "string"
+        f.description shouldBe None
+      }
+    }
+
+
+    model("NewPet") { m =>
+      m.plural shouldBe "NewPets"
+      m.description shouldBe None
+      m.deprecation shouldBe None
+      m.fields.map(_.name) shouldBe Seq("name", "tag")
+
+      field(m, "name") { f =>
+        f.required shouldBe true
+        f.`type` shouldBe "string"
+        f.description shouldBe None
+      }
+
+      field(m, "tag") { f =>
+        f.required shouldBe false
+        f.`type` shouldBe "string"
+        f.description shouldBe None
+      }
+    }
+
+  }
 }
