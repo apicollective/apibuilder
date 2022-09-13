eeda2cbde7cf80e088bd39be17e827a681bfb289
Michael Bryzek <mike@flow.io>
2022-09-13 16:45:24 -0400

    wip

--- core/src/main/scala/core/DuplicateJsonParser.scala
+++ core/src/main/scala/core/DuplicateJsonParser.scala
@@ -1,33 +1,22 @@
 package core
 
-import org.typelevel.jawn.Facade.SimpleFacade
-import org.typelevel.jawn.Parser
-import play.api.libs.json._
+import io.circe.jawn.JawnParser
 
-import scala.util.{Failure, Success}
+//  scala> parser.decode[Map[String, Int]]("""{"a":1,"a":2}""")
 
 object DuplicateJsonParser {
-  implicit object PlayJsonFacade extends SimpleFacade[JsValue] {
-    override def jnull = JsNull
-    override def jfalse = JsFalse
-    override def jtrue = JsTrue
-
-    override def jnum(s: CharSequence, decIndex: Int, expIndex: Int): JsValue = {
-      JsNumber(BigDecimal(String.valueOf(s)))
-    }
-    override def jstring(s: CharSequence): JsValue = JsString(String.valueOf(s))
-    override def jarray(vs: List[JsValue]) = JsArray(vs)
-    override def jobject(vs: Map[String, JsValue]) = JsObject(vs)
-  }
 
   def foo(value: String): Seq[String] = {
-    new JawnParser()
-    Parser.parseFromString(value) match {
-      case Success(js) => {
+    val parser = JawnParser(allowDuplicateKeys = false)
+    parser.decode(value) match {
+      case Left(er) => {
+        println(s"ERR: $er")
+        Nil
+      }
+      case Right(js) => {
         println(s"JS: $js")
         Nil
       }
-      case Failure(_) => Nil
     }
   }
 }
