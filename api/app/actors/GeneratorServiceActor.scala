bfa672a3acaa104a5da420957317876fca9b5dd1
Michael Bryzek <mike@flow.io>
2022-10-23 13:38:49 -0400

    Cleanup test logs

--- api/app/actors/GeneratorServiceActor.scala
+++ api/app/actors/GeneratorServiceActor.scala
@@ -4,10 +4,10 @@ import akka.actor.{ActorLogging, ActorSystem}
 import lib.Pager
 import db.Authorization
 import db.generators.ServicesDao
-import play.api.Logger
+import play.api.{Environment, Logger, Mode}
 import akka.actor.Actor
-import java.util.UUID
 
+import java.util.UUID
 import util.GeneratorServiceUtil
 
 import scala.concurrent.duration._
@@ -26,10 +26,15 @@ object GeneratorServiceActor {
 class GeneratorServiceActor @javax.inject.Inject() (
   system: ActorSystem,
   servicesDao: ServicesDao,
-  util: GeneratorServiceUtil
+  util: GeneratorServiceUtil,
+  env: Environment
 ) extends Actor with ActorLogging with ErrorHandler {
 
   private[this] implicit val ec = system.dispatchers.lookup("generator-service-actor-context")
+  private[this] val inTestEnv = env.mode match {
+    case Mode.Test => true
+    case Mode.Dev | Mode.Prod => false
+  }
 
   system.scheduler.scheduleAtFixedRate(1.hour, 1.hour, self, GeneratorServiceActor.Messages.SyncAll)
 
@@ -54,7 +59,10 @@ class GeneratorServiceActor @javax.inject.Inject() (
             log.info(s"[GeneratorServiceActor] Service[${service.guid}] at uri[${service.uri}] synced")
           }
           case Failure(ex) => {
-            log.error(s"[GeneratorServiceActor] Service[${service.guid}] at uri[${service.uri}] failed to sync: ${ex.getMessage}", ex)
+            ex match {
+              case _: java.net.UnknownHostException if inTestEnv => // ignore
+              case _ => log.error(s"[GeneratorServiceActor] Service[${service.guid}] at uri[${service.uri}] failed to sync: ${ex.getMessage}", ex)
+            }
           }
         }
       }
