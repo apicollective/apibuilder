3effc7fb496effa8d2700d5a4f64e2f0ba2129ec
Michael Bryzek <mike@flow.io>
2022-11-28 17:44:33 -0500

    wip

--- api/test/modules/TestClientModule.scala
+++ api/test/modules/TestClientModule.scala
@@ -7,7 +7,7 @@ import play.api.inject.Module
 class TestClientModule extends Module {
 
   def bindings(env: Environment, conf: Configuration) = {
-    assert(env.mode == Mode.Prod, s"Mode expected to be '${Mode.Test}' and not '${env.mode}' for class[${getClass.getName}]")
+    assert(env.mode == Mode.Test, s"Mode expected to be '${Mode.Test}' and not '${env.mode}' for class[${getClass.getName}]")
     Seq(
       bind[GeneratorClientFactory].to[TestGeneratorClientFactory]
     )
