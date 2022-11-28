755e0f3126e11771ea442e7d7c27bb4c686b34f2
Michael Bryzek <mike@flow.io>
2022-11-28 17:49:14 -0500

    wip

--- api/test/util/GeneratorServiceUtilSpec.scala
+++ api/test/util/GeneratorServiceUtilSpec.scala
@@ -1,9 +1,11 @@
 package util
 
+import db.Authorization
 import db.generators.GeneratorHelpers
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 
+import java.util.UUID
 import scala.concurrent.ExecutionContext.Implicits.global
 
 class GeneratorServiceUtilSpec extends PlaySpec with GuiceOneAppPerSuite with GeneratorHelpers {
@@ -13,6 +15,18 @@ class GeneratorServiceUtilSpec extends PlaySpec with GuiceOneAppPerSuite with Ge
   "syncAll" in {
     val s1 = createGeneratorService()
     val s2 = createGeneratorService()
+
+    def find(serviceGuid: UUID) = {
+      generatorsDao.findAll(
+        authorization = Authorization.All,
+        serviceGuid = Some(serviceGuid),
+        limit = 1
+      ).headOption
+    }
+
+    find(s1.guid) mustBe None
+
     util.syncAll(pageSize = 10)
+    find(s1.guid).value.service.guid mustBe s1.guid
   }
 }
