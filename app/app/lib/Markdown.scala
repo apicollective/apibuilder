4e9ac335771e62a5416c02ed5198784b1dad2ba7
Michael Bryzek <mbryzek@alum.mit.edu>
2024-04-05 12:51:30 -0400

    wip

--- app/app/lib/Markdown.scala
+++ app/app/lib/Markdown.scala
@@ -1,6 +1,8 @@
 package lib
 
-import org.pegdown.{Extensions, PegDownProcessor}
+import com.vladsch.flexmark.html.HtmlRenderer
+import com.vladsch.flexmark.parser.Parser
+import com.vladsch.flexmark.util.data.MutableDataSet
 
 /**
   * Wrapper on play config testing for empty strings and standardizing
@@ -12,11 +14,16 @@ object Markdown {
     value: Option[String],
     default: String = ""
   ): String = {
-    value.map { toHtml(_) }.getOrElse(default)
+    value.map { toHtml }.getOrElse(default)
   }
 
+  private[this] val options = new MutableDataSet()
+  private[this] val parser = Parser.builder(options).build
+  private[this] val renderer = HtmlRenderer.builder(options).build
+
   def toHtml(value: String): String = {
-    (new PegDownProcessor(Extensions.ALL)).markdownToHtml(value)
+    val document = parser.parse(value)
+    renderer.render(document)
   }
 
 }
