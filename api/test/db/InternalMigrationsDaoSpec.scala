967aa951cd4c561174e860afa2f5f5d25a5faa26
Michael Bryzek <mbryzek@alum.mit.edu>
2024-05-28 20:48:09 -0400

    wip

--- api/test/db/InternalMigrationsDaoSpec.scala
+++ api/test/db/InternalMigrationsDaoSpec.scala
@@ -1,20 +1,25 @@
 package db
 
 import db.generated.MigrationsDao
+import io.flow.postgresql.Query
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
-import play.api.db.Database
 
-class InternalMigrationsDaoSpec extends PlaySpec with GuiceOneAppPerSuite with db.Helpers {
+class InternalMigrationsDaoSpec extends PlaySpec with GuiceOneAppPerSuite with db.Helpers with DbUtils {
 
-  private[this] val db: Database = injector.instanceOf[Database]
   private[this] def internalMigrationsDao: InternalMigrationsDao = injector.instanceOf[InternalMigrationsDao]
   private[this] def generatedMigrationsDao: MigrationsDao = injector.instanceOf[MigrationsDao]
 
-  "foo" in {
+  "queueVersions" in {
     val version = createVersion()
     println(s"Created version: ${version.guid}")
     def count = generatedMigrationsDao.findAll(versionGuid = Some(version.guid), limit = None).length
+
+    execute(
+      Query("update cache.services set deleted_at=now(), deleted_by_guid={user_guid}::uuid")
+        .equals("version_guid", version.guid)
+        .bind("user_guid", testUser.guid)
+    )
 
     count mustBe 0
     internalMigrationsDao.queueVersions()
