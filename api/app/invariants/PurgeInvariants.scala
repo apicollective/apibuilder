506d47d75f504421886f67d68149fe8b48bc7668
Michael Bryzek <mbryzek@alum.mit.edu>
2024-06-06 16:16:55 -0400

    Adjust invariants based on purge job

--- api/app/invariants/PurgeInvariants.scala
+++ api/app/invariants/PurgeInvariants.scala
@@ -3,15 +3,18 @@ package invariants
 import io.flow.postgresql.Query
 
 object PurgeInvariants {
+  private[this] case class PurgeTable(name: String, retentionMonths: Int)
   private[this] val Tables = Seq(
-    "organizations", "applications", "versions"
+    PurgeTable("organizations", 12),
+    PurgeTable("applications", 6),
+    PurgeTable("versions", 6)
   )
 
   val all: Seq[Invariant] = Tables.map { t =>
     Invariant(
-      s"old_deleted_records_purged_from_$t",
+      s"old_deleted_records_purged_from_${t.name}",
       Query(
-        s"select count(*) from $t where deleted_at < now() - interval '1 year'"
+        s"select count(*) from ${t.name} where deleted_at < now() - interval '${t.retentionMonths + 1} months'"
       )
     )
   }
