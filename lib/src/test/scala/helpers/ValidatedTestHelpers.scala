a71d3648f0b50c1ec91bbe1c47815409c605a48d
Michael Bryzek <mbryzek@alum.mit.edu>
2024-07-04 10:27:05 -0400

    wip

--- lib/src/test/scala/helpers/ValidatedTestHelpers.scala
+++ lib/src/test/scala/helpers/ValidatedTestHelpers.scala
@@ -5,10 +5,10 @@ import cats.data.ValidatedNec
 
 trait ValidatedTestHelpers {
 
-  def expectValid[T](r: ValidatedNec[?, T]): T = {
-    r match {
-      case Valid(o) => o
-      case Invalid(errors) => sys.error(s"Expected valid but was invalid: ${errors.toNonEmptyList}")
+  def expectValid[S, T](value: ValidatedNec[S, T]): T = {
+    value match {
+      case Invalid(e) => sys.error(s"Expected valid but got: ${e.toNonEmptyList.toList.mkString(", ")}")
+      case Valid(v) => v
     }
   }
 
